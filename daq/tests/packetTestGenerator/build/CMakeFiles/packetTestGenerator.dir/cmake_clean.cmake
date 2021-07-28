de9a3bcdd3f4b165a794b37a028ffbee85873aea
Ryan Lee <ryanl@berkeley.edu>
2021-07-28 11:04:06 -0700

    Moved Hashpipe related code into daq directory and repurpose directory into panoseti directory with proper subdirectories

---
+++ daq/tests/packetTestGenerator/build/CMakeFiles/packetTestGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/packetTestGenerator.dir/main.cpp.o"
+  "packetTestGenerator.pdb"
+  "packetTestGenerator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/packetTestGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
