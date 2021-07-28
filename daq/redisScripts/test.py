fc064a0bf80b040cfc4085bf65f6a5d4433b033e
Ryan Lee <ryanl@berkeley.edu>
2021-07-28 11:10:40 -0700

    Moved redisScripts into daq

---
+++ daq/redisScripts/test.py
@@ -0,0 +1,17 @@
+from panosetiSIconvert import HKconvert
+
+HKconv = HKconvert()
+
+HKconv.changeUnits('')
+print('\n')
+HKconv.changeUnits('V')
+HKconv.changeUnits('A')
+print('\n')
+HKconv.changeUnits('mV')
+HKconv.changeUnits('mA')
+print('\n')
+HKconv.changeUnits('uV')
+HKconv.changeUnits('uA')
+print('\n')
+HKconv.changeUnits('nV')
+HKconv.changeUnits('nA')
