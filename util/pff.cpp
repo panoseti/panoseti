9a7f7bd2bc84d4050254cbd736b98aea7dd00cef
David Anderson <davea@ssl.berkeley.edu>
2021-08-31 16:12:34 -0700

    add pff.cpp

---
+++ util/pff.cpp
@@ -0,0 +1,138 @@
+#include <string.h>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+#include "pff.h"
+
+using std::string;
+using std::vector;
+
+void DIRNAME_INFO::make(string &s) {
+    char buf[1024], tbuf[256];
+
+    time_t x = (time_t)start_time;
+    struct tm* tm = localtime(&x);
+    strftime(tbuf, sizeof(tbuf), "%a_%b_%d_%T_%Y", tm);
+    sprintf(buf, "obs=%s,st=%s", observatory, tbuf);
+    s = buf;
+}
+
+struct NV_PAIR {
+    char name[64], value[256];
+    int parse(const char *s) {
+        char *p = (char*)strchr(s, '=');
+        if (!p) return -1;
+        *p = 0;
+        strcpy(name, s);
+        strcpy(value, p+1);
+        return 0;
+    }
+};
+
+void split_comma(char *name, vector<string> &pieces) {
+    char *p = name;
+    while (1) {
+        char *q = strchr(p, ',');
+        if (!q) break;
+        *q = 0;
+        pieces.push_back(string(p));
+        p = q+1;
+    }
+    pieces.push_back(string(p));
+}
+
+int DIRNAME_INFO::parse(char* name) {
+    vector<string> pieces;
+    split_comma(name, pieces);
+    for (int i=0; i<pieces.size(); i++) {
+        NV_PAIR nvp;
+        int retval = nvp.parse(pieces[i].c_str());
+        if (retval) {
+            fprintf(stderr, "bad filename component: %s\n", pieces[i].c_str());
+        }
+        if (!strcmp(nvp.name, "obs")) {
+            strcpy(observatory, nvp.value);
+        } else if (!strcmp(nvp.name, "st")) {
+            struct tm tm;
+            char *p = strptime(nvp.value, "%a_%b_%d_%T_%Y", &tm);
+            time_t t = mktime(&tm);
+            start_time = (double)t;
+        } else {
+            fprintf(stderr, "unknown dirname key: %s\n", nvp.name);
+        }
+    }
+    return 0;
+}
+
+void FILENAME_INFO::make(string &s) {
+    char buf[1024], tbuf[256];
+
+    time_t x = (time_t)start_time;
+    struct tm* tm = localtime(&x);
+    strftime(tbuf, sizeof(tbuf), "%a_%b_%d_%T_%Y", tm);
+    sprintf(buf, "st=%s,dp=%d,bpp=%d,dome=%d,module=%d,seqno=%d.pff",
+        tbuf, data_product, bytes_per_pixel, dome, module, seqno
+    );
+    s = buf;
+}
+
+int FILENAME_INFO::parse(char* name) {
+    vector<string> pieces;
+    char* p = strrchr(name, '.');   // trim .pff
+    if (!p) return 1;
+    *p = 0;
+    split_comma(name, pieces);
+    for (int i=0; i<pieces.size(); i++) {
+        NV_PAIR nvp;
+        int retval = nvp.parse(pieces[i].c_str());
+        if (retval) {
+            fprintf(stderr, "bad filename component: %s\n", pieces[i].c_str());
+        }
+        if (!strcmp(nvp.name, "st")) {
+            struct tm tm;
+            char *p = strptime(nvp.value, "%a_%b_%d_%T_%Y", &tm);
+            time_t t = mktime(&tm);
+            start_time = (double)t;
+        } else if (!strcmp(nvp.name, "dp")) {
+            data_product = (DATA_PRODUCT)atoi(nvp.value);
+        } else if (!strcmp(nvp.name, "dome")) {
+            dome = atoi(nvp.value);
+        } else if (!strcmp(nvp.name, "mod")) {
+            module = atoi(nvp.value);
+        } else if (!strcmp(nvp.name, "seqno")) {
+            seqno = atoi(nvp.value);
+        } else {
+            fprintf(stderr, "unknown filename key: %s\n", nvp.name);
+        }
+    }
+    return 0;
+}
+
+#if 1
+int main(int, char**) {
+    DIRNAME_INFO di;
+    strcpy(di.observatory, "Palomar");
+    di.start_time = time(0);
+    string s;
+    di.make(s);
+    printf("dir name: %s\n", s.c_str());
+
+    FILENAME_INFO fi;
+    fi.start_time = time(0);
+    fi.data_product = DP_PH;
+    fi.bytes_per_pixel = 2;
+    fi.dome = 0;
+    fi.module=14;
+    fi.seqno = 5;
+    fi.make(s);
+    printf("file name: %s\n", s.c_str());
+
+    char buf[256];
+    strcpy(buf, "obs=Palomar,st=Fri_Aug_27_15:21:46_2021");
+    di.parse(buf);
+
+    strcpy(buf, "st=Fri_Aug_27_15:21:46_2021,dp=1,bpp=2,dome=0,module=14,seqno=5.pff");
+    fi.parse(buf);
+}
+#endif
