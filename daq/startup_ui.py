08c9d273c719cd8e9c4ddd32ebe5cbf92f2b5867
Ryan Lee <ryanl@berkeley.edu>
2021-11-15 16:06:09 -0800

    Working scriptable data acqusition software

--- daq/startup_ui.py
+++ daq/startup_ui.py
@@ -1,20 +1,80 @@
 import tkinter as tk
 from tkinter import StringVar, filedialog
+from threading import Thread
+import subprocess
+import time
+import signal
+
+class DAQ:
+    def __init__(self) -> None:
+        self.process = None
+        self.str_val : StringVar = None
+        self.update_thread = Thread(target=self._threaded_update)
+
+    def run(self) -> int:
+        self.process = subprocess.Popen(["hashpipe", "-p", "HSD_hashpipe", "-I", "0", "-o",
+"BINDHOST=\"0.0.0.0\"", "-o", "MAXFILESIZE=500", "-o", "SAVELOC=./", "-o",
+"CONFIG=./module.config", "HSD_net_thread", "HSD_compute_thread",  "HSD_output_thread"], shell=False, stdout=subprocess.PIPE)
+    
+    def stop(self) -> int:
+        self.process.send_signal(signal.SIGINT)
+        self.process.wait()
+
+    def is_running(self):
+        if self.process == None:
+            return False
+        
+        if self.process.poll() == None:
+            return True
+        
+        if self.process.poll() < 0:
+            return False
+            
+    def get_stdout_update(self):
+        return self.process.stdout.readline().decode("utf-8")
+    
+    def update_to_StringVar(self, str_val : StringVar):
+        if self.update_thread != None and self.update_thread.is_alive():
+            print("Returned")
+            return
+        self.update_thread = Thread(target=self._threaded_update)
+        self.str_val = str_val
+        self.update_thread.start()
+    
+    def update_to_stdout(self):
+        if self.update_thread != None and self.update_thread.is_alive():
+            return
+        self.update_thread = Thread(target=self._threaded_update)
+        self.str_val = None
+        self.update_thread.start()
+
+    def _threaded_update(self):
+        if self.process == None:
+            return
+        
+        while self.process.poll() == None:
+            if self.str_val == None:
+                print(self.get_stdout_update())
+            else:
+                self.str_val.set(self.str_val.get() + self.get_stdout_update())
+            time.sleep(0.1)
+
 
 class PanosetiMainControl:
     def __init__(self) -> None:
         self.main_window = tk.Tk()
+        self.main_window.geometry("1280x720")
         self.main_frame = tk.Frame(self.main_window)
-
         self.status_frame = tk.Frame(self.main_frame)
 
-        self.hashpipe_frame = tk.Frame(self.status_frame)
-        self.hashpipe_message = StringVar()
-        tk.Message(self.hashpipe_frame, textvariable=self.hashpipe_message).pack()
-        self.hashpipe_message.set("Hashpipe Message")
-        self.hashpipe_message.set(self.hashpipe_message.get() + "Changed")
-        tk.Button(self.hashpipe_frame, text="Hashpipe").pack()
-        self.hashpipe_frame.pack(side=tk.LEFT)
+        self.daq_frame = tk.Frame(self.status_frame)
+        self.daq_process = DAQ()
+        self.daq_message = StringVar()
+        tk.Message(self.daq_frame, textvariable=self.daq_message).pack()
+        self.daq_button_message = StringVar()
+        self.daq_button_message.set("Start Data Acqusition")
+        self.daq_button = tk.Button(self.daq_frame, textvariable=self.daq_button_message, command=self.daq).pack()
+        self.daq_frame.pack(side=tk.LEFT)
 
         self.redis_frame = tk.Frame(self.status_frame)
         tk.Message(self.redis_frame, text="Redis Message").pack()
@@ -62,9 +122,26 @@ class PanosetiMainControl:
 
     def exit(self):
         exit(0)
+    
+    def daq(self):
+        if self.daq_process.is_running() or self.daq_process.update_thread.is_alive():
+            self.daq_process.stop()
+            if not self.daq_process.is_running():
+                self.daq_button_message.set("Start Data Acqusition")
+            return
+        
+        self.daq_message.set("")
+        self.daq_process.run()
+        self.daq_process.update_to_StringVar(self.daq_message)
+        #self.daq_process.update_to_stdout()
+        self.daq_button_message.set("Stop Data Acqusition")
+    
+    def test_running(self):
+        print(self.daq_process.is_running())
+        
 
 
-PanosetiMainControl()
 
-
-tk.mainloop()
+if __name__ == "__main__":
+    PanosetiMainControl()
+    tk.mainloop()
