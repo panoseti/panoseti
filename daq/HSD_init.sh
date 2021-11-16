d73a0a9cc8aeea63a89b7352772e49935a779b4c
Ryan Lee <ryanl@berkeley.edu>
2021-11-15 16:07:50 -0800

    Added OBS to startup script

--- daq/HSD_init.sh
+++ daq/HSD_init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hashpipe -p HSD_hashpipe -I 0 -o BINDHOST="0.0.0.0" -o MAXFILESIZE=500 -o SAVELOC="/media/panosetigraph/4TB_SSD" -o CONFIG="./module.config" HSD_net_thread HSD_compute_thread  HSD_output_thread
+hashpipe -p HSD_hashpipe -I 0 -o BINDHOST="0.0.0.0" -o MAXFILESIZE=500 -o SAVELOC="./" -o CONFIG="./module.config" -o OBS="LICK" HSD_net_thread HSD_compute_thread  HSD_output_thread
