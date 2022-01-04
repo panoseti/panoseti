9ed99e8d79a62b625a1ab0eb4366c4ac4fb0e20c
Ryan Lee <ryanl@berkeley.edu>
2022-01-04 15:16:51 -0800

    Make storing of influx modular

--- daq/redisScripts/storeInfluxDB.py
+++ daq/redisScripts/storeInfluxDB.py
@@ -1,3 +1,4 @@
+from os import write
 from influxdb import InfluxDBClient
 import redis
 import time
@@ -7,6 +8,8 @@ OBSERVATORY = 'Lick'
 DATATYPE_FORMAT = {'housekeeping': re.compile("Quabo_[0-9]*"),
     'GPS': re.compile("GPS.*"),
     'whiterabbit': re.compile("WRSWITCH.*")}
+#List of keys with the time stamp values
+key_timestamps = {}
 
 
 def influx_init():
@@ -15,6 +18,21 @@ def influx_init():
     client.create_database('metadata')
 
     return r, client
+
+def get_updated_redis_keys(r:redis.Redis, key_timestamps:dict):
+    avaliable_keys = [key.decode("utf-8") for key in r.keys('*')]
+    list_of_updates = []
+    for key in avaliable_keys:
+        try:
+            compUTC = r.hget(key, 'Computer_UTC')
+            if compUTC == None:
+                continue
+            if key in key_timestamps and key_timestamps[key] == compUTC.decode("utf-8"):
+                continue
+            list_of_updates.append(key)
+        except redis.ResponseError:
+            pass
+    return list_of_updates
 
 def get_datatype(redis_key):
     for key in DATATYPE_FORMAT.keys():
@@ -37,28 +55,36 @@ def write_influx(client:InfluxDBClient, key:str, data_fields:dict, datatype:str)
     ]
     client.write_points(json_body)
 
+def write_redis_to_influx(client:InfluxDBClient, r:redis.Redis, redis_keys:list, key_timestamps:dict):
+    for rkey in redis_keys:
+        redis_value = r.hgetall(rkey)
+        data_fields = { k.decode('utf-8'): redis_value[k].decode('utf-8') for k in redis_value.keys() }
+        write_influx(client, rkey, data_fields, get_datatype(rkey))
+        key_timestamps[rkey] = data_fields['Computer_UTC']
+
 def main():
     r, client = influx_init()
     key_timestamps = {}
     while True:
-        avaliable_keys = [key.decode("utf-8") for key in r.keys('*')]
-        for key in avaliable_keys:
-            try:
-                compUTC = r.hget(key, 'Computer_UTC')
-                if compUTC == None:
-                    continue
-                if key in key_timestamps and key_timestamps[key] == compUTC:
-                    continue
-                
-                redis_set = r.hgetall(key)
-                data_fields = {}
-                for rkey in redis_set:
-                    data_fields[rkey.decode("utf-8")] = redis_set[rkey].decode("utf-8")
-                
-                write_influx(client, key, data_fields, get_datatype(key))
-                key_timestamps[key] = compUTC
-            except redis.ResponseError:
-                pass
+        write_redis_to_influx(client, r, get_updated_redis_keys(r, key_timestamps), key_timestamps)
+        #avaliable_keys = [key.decode("utf-8") for key in r.keys('*')]
+        #for key in avaliable_keys:
+        #    try:
+        #        compUTC = r.hget(key, 'Computer_UTC')
+        #        if compUTC == None:
+        #            continue
+        #        if key in key_timestamps and key_timestamps[key] == compUTC:
+        #            continue
+        #        
+        #        redis_set = r.hgetall(key)
+        #        data_fields = {}
+        #        for rkey in redis_set:
+        #            data_fields[rkey.decode("utf-8")] = redis_set[rkey].decode("utf-8")
+        #        
+        #        write_influx(client, key, data_fields, get_datatype(key))
+        #        key_timestamps[key] = compUTC
+        #    except redis.ResponseError:
+        #        pass
         time.sleep(1)
 
 if __name__ == "__main__":
