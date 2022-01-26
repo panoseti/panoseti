5a801ea7b7cf5b8a8413d7e47f8ee374a0629942
David Anderson <davea@berkeley.edu>
2022-01-25 21:08:26 -0800

    Change/fix the way the mechanism for remotely starting hashpipe on a DAQ node.
    The head node passes to start_daq.py:
        - the list of module IDs to expect data from
        - the max file size
        - the run dir name
    
    start_daq.py then creates
        - module.config (list of module IDs)
        - a shell script to run hashpipe, passing it run dir name and max file size
    It runs the shell script, uses "pgrep" to find the PID of the hashpipe process,
    and writes that to a file.
    
    This is the only way that Ryan and I could figure out to run hashpipe
    where the python script exits, but hashpipe keeps running,
    and another process can kill it later.
    
    TODO:
    - change start.py accordingly
    - change hashpipe to not create run dir, and not create HK stuff

--- control/start_daq.py
+++ control/start_daq.py
@@ -1,61 +1,93 @@
 #! /usr/bin/env python3
 
-# start_daq.py dirname
-# This script is run (remotely) on a DAQ node to start recording
-# The run directory has already been created
-# - start the Hashpipe process
-# - record the PID and dirname in a file
+# start_daq.py --run_dir dirname --max_file_size N --module_id M1 ... --module_id Mn
 #
-# On success, print OK.  Otherwise print an error message
+# This script is run (remotely) on a DAQ node to start recording.
+# The run directory already exists.
+# - create a shell script to run hashpipe (see below)
+# - create a module.config telling hashpipe what modules to listen for
+# - run/detach the shell script to start hashpipe
+# - use pgrep to find the PID of the hashpipe process
+# - record the PID in a file, so that stop_daq.py can kill it later.
+#
+# This is arcane, but I can't find a simpler way.
+# If we run hashpipe directly from this script, it dies when the script exits.
+# If we run hashpipe via a shell script and kill the shell process,
+# hashpipe keeps running.
 
-import sys, os, subprocess
+import sys, os, subprocess, time
 
 pid_filename = 'daq_pid'
 dirname_filename = 'daq_dirname'
     # stores name of current run
 
 def main():
-    if len(sys.argv) != 2:
-        print("usage: start_daq.py dirname")
-        return
-    dirname = sys.argv[1]
-
+    argv = sys.argv
+    i = 1
+    max_file_size = None
+    run_dir = None
+    module_ids = []
+    while i<len(argv):
+        if argv[i] == '--run_dir':
+            i += 1
+            run_dir = argv[i]
+        elif argv[i] == '--max_file_size':
+            i += 1
+            max_file_size = int(argv[i])
+        elif argv[i] == '--module_id':
+            i += 1
+            module_ids.append(int(argv[i]))
+        i += 1
+    if not run_dir:
+        raise Exception('no run dir specified')
+    if not max_file_size:
+        raise Exception('no max file size specified')
+    if len(module_ids) == 0:
+        raise Exception('no module IDs specified')
+    if not os.path.isdir(run_dir):
+        raise Exception("run dir doesn't exist")
     if os.path.exists(pid_filename):
-        print("PID file exists; run stop_daq.py")
-        #return
+        raise Exception("PID file exists; run stop_daq.py")
 
     # record the run name in a file
 
-    try:
-        f = open(dirname_filename, 'w')
-    except Exception as e:
-        print(e)
-        print("can't create run name file")
-        return
-    f.write(dirname)
+    f = open(dirname_filename, 'w')
+    f.write(run_dir)
     f.close()
 
-    # run the DAQ program
+    # create module.config
 
-    try:
-        process = subprocess.Popen(
-            ['./record_time.py', dirname], start_new_session=True,
-            close_fds=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-        )
-    except:
-        print("can't launch DAQ process")
-        return
-
-    # write its PID to a file
-
-    try:
-        f = open(pid_filename, 'w')
-    except:
-        print("can't create PID file")
-        return
-    f.write(str(process.pid))
+    f = open('module.config', 'w')
+    for id in module_ids:
+        f.write('%d\n'%id)
     f.close()
 
-    print('OK')
+    # create the run script
+
+    f = open('run_hashpipe.sh', 'w')
+    f.write('hashpipe -p ./HSD_hashpipe.so -I 0 -o BINDHOST="0.0.0.0" -o MAXFILESIZE=%d -o SAVELOC="%s" -o CONFIG="./module.config" -o OBS="LICK" HSD_net_thread HSD_compute_thread  HSD_output_thread'%(max_file_size, run_dir))
+    f.close()
+
+    # run the script
+
+    process = subprocess.Popen(
+        ['bash', 'run_hashpipe.sh'], start_new_session=True,
+        close_fds=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    )
+
+    # use pgrep to find the PID of the hashpipe process
+
+    pid = process.pid
+    while True:
+        result = subprocess.run(['pgrep', '-P', str(pid)], stdout=subprocess.PIPE)
+        if result != '': break
+        time.sleep(1)
+    child_pid = int(result.stdout)
+
+    # write it to a file
+
+    f = open(pid_filename, 'w')
+    f.write(str(child_pid))
+    f.close()
 
 main()
