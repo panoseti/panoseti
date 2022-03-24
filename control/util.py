70134b2d1ed7605e1f9d0acb9bc08ed295b41422
Wei Liu <liuwei_berkeley@berkeley.edu>
2022-03-24 16:28:20 -0700

    update util.py, checking username for killing hk recorder.

--- control/util.py
+++ control/util.py
@@ -220,7 +220,7 @@ def kill_hashpipe():
 
 def kill_hk_recorder():
     for p in psutil.process_iter():
-        if (hk_recorder_name in p.cmdline()) and p.username() == os.getlogin():
+        if (hk_recorder_name in p.cmdline()) and (p.username() == os.getenv('USER')):
             os.kill(p.pid, signal.SIGKILL)
 
 def disk_usage(dir):
