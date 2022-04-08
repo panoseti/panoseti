b7f6014e6720e32d44ffd139aec867e3982397e5
David Anderson <davea@ssl.berkeley.edu>
2022-04-07 17:06:22 -0700

    - config.py --maroc_config:     Set the DAC2 values if we're doing both image and PH     Use the proper pixel-coordinate conversions     (depending on if old or new quabo wiring) - finish the pixel-coordinate stuff (util.pixel_coords.py)

--- control/config_file.py
+++ control/config_file.py
@@ -13,8 +13,10 @@ quabo_info_filename = '../quabos/quabo_info.json'
 detector_info_filename = '../quabos/detector_info.json'
 quabo_calib_filename = '../quabos/quabo_calib_%s.json'
 
+# list of config files copied to data dir
 config_file_names = [
-    obs_config_filename, daq_config_filename, data_config_filename, quabo_uids_filename
+    obs_config_filename, daq_config_filename, data_config_filename,
+    quabo_uids_filename
 ]
 
 # assign sequential numbers to domes,
