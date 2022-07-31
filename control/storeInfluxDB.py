12610a474cbec7f70e4a3543b955a4b27c6af1af
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2022-07-31 15:13:14 -0700

    storeInfluxdb.py: fix ups not found issue.

--- control/storeInfluxDB.py
+++ control/storeInfluxDB.py
@@ -21,7 +21,7 @@ DATATYPE_FORMAT = {
     'housekeeping': re.compile("QUABO_\\d*"),
     'GPS': re.compile("GPS.*"),
     'whiterabbit': re.compile("WRSWITCH.*"),
-    'outlet': re.compile("UPS_.*")
+    'outlet': re.compile("UPS.*")
 }
 # List of keys with the time stamp values
 key_timestamps = {}
