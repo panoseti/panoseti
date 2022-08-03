6ae85174d360445ea170679f5ec60e734b7949f3
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2022-08-03 14:39:56 -0700

    Change timestamp calculation.

--- analysis/coincidence.py
+++ analysis/coincidence.py
@@ -18,12 +18,18 @@ sys.path.append('../control')
 import config_file
 
 # Default data
-DATA_IN_DIR = '/Users/nico/Downloads/720_ph_12pe'
+DATA_IN_DIR = '/Users/nico/Downloads/719_ph_12pe'
 DATA_OUT_DIR = '.'
 
-fname_a = 'start_2022-07-21T06_03_03Z.dp_ph16.bpp_2.dome_0.module_1.seqno_0.pff' # astrograph 1
+
+fname_a = 'start_2022-07-20T06_44_48Z.dp_ph16.bpp_2.dome_0.module_1.seqno_0.pff' # astrograph 1
+fname_b = 'start_2022-07-20T06_44_48Z.dp_ph16.bpp_2.dome_0.module_254.seqno_0.pff' # astrograph 2
+#fname_b = 'start_2022-07-20T06_44_48Z.dp_ph16.bpp_2.dome_0.module_3.seqno_0.pff' # nexdome
+
+# July 20
+#fname_a = 'start_2022-07-21T06_03_03Z.dp_ph16.bpp_2.dome_0.module_1.seqno_0.pff' # astrograph 1
 #fname_a = 'start_2022-07-21T06_03_03Z.dp_ph16.bpp_2.dome_0.module_254.seqno_0.pff' # astrograph 2
-fname_b = 'start_2022-07-21T06_03_03Z.dp_ph16.bpp_2.dome_0.module_3.seqno_0.pff' # nexdome
+#fname_b = 'start_2022-07-21T06_03_03Z.dp_ph16.bpp_2.dome_0.module_3.seqno_0.pff' # nexdome
 
 fpath_a = f'{DATA_IN_DIR}/{fname_a}'
 fpath_b = f'{DATA_IN_DIR}/{fname_b}'
@@ -41,6 +47,7 @@ def process_file(fpath, collect: list):
     Assumes that fpath is a ph file."""
     img_size = 16
     bytes_per_pixel = 2
+    print(f'Reading {fpath}')
     with open(fpath, 'rb') as f:
         frame_num = 0
         while True:
@@ -50,23 +57,22 @@ def process_file(fpath, collect: list):
                 j = pff.read_json(f)
             except Exception as e:
                 if repr(e)[:26] == "Exception('bad type code',":
-                    print('\n\tReached EOF')
+                    print('\n\tReached EOF.')
                     break
             if not j:
-                print('\n\tReached EOF')
+                print('\n\tReached EOF.')
                 break
-            print(f'Processed up to frame {frame_num}.', end='\r')
+            print(f'\tProcessed up to frame {frame_num}.', end='\r')
             c = json.loads(j.encode())
             img = pff.read_image(f, img_size, bytes_per_pixel)
             collect.append((frame_num, c, img))
             frame_num += 1
 
 
-def get_timestamp_lists(a_path, b_path):
+def get_frames(a_path, b_path):
     a_list, b_list = list(), list()
     process_file(a_path, a_list)
     process_file(b_path, b_list)
-
     return a_list, b_list
 
 
@@ -74,53 +80,48 @@ def get_timestamp(j):
     """Returns a tuple of [pkt_utc], [pkt_nsec]."""
     pkt_utc = j[1]['pkt_utc']
     pkt_nsec = j[1]['pkt_nsec']
-    return pkt_utc, pkt_nsec
+    return pkt_utc * 10**9 + pkt_nsec
 
 
 def get_timestamp_ns_diff(a, b):
     """Returns a tuple (diff in pkt_utc, diff in pkt_nsec)."""
     a_time, b_time = get_timestamp(a), get_timestamp(b)
-    a_utc, a_nsec = a_time[0], a_time[1]
-    b_utc, b_nsec = b_time[0], b_time[1]
-    return a_utc - b_utc, a_nsec - b_nsec
+    return a_time - b_time
 
 
 def is_coincident(a, b, max_time_diff):
     """Returns True iff the absolute difference between the timestamps of a and b is less than or equal to max_time_diff."""
-    pkt_utc_diff, pkt_nsec_diff = get_timestamp_ns_diff(a, b)
-    within_threshold = pkt_utc_diff == 0 and abs(pkt_nsec_diff) <= max_time_diff
-
+    within_threshold = abs(get_timestamp_ns_diff(a, b)) <= max_time_diff
     return within_threshold
 
 
 def a_after_b(a, b):
     """Returns True iff the timestamp of a is greater than the timestamp of b."""
     diff = get_timestamp_ns_diff(a, b)
-    utc_diff, nsec_diff = diff[0], diff[1]
-    return (utc_diff > 0) or (utc_diff == 0 and nsec_diff >= 0)
-
+    return diff > 0
 
 def do_search(a_path, b_path, max_time_diff, threshold_pe):
     """
-    Identify ph frames in 2 ph files that have timestamps with a difference of no more than 100ns.
+    Identify all pairs of frames from 2 ph files that have timestamps with a difference of no more than 100ns.
+    Assumes that ph frames are in monotonically increasing chronological order.
     Returns a list of [frame number], [json data] pairs.
     """
     pairs = list()
-    a_list, b_list = get_timestamp_lists(a_path, b_path)
-    '''
-    for x in a_list[:15]:
-        print(x)
-    print("*"*5)
-    for x in b_list[:15]:
-        print(x)
-    '''
+    a_frames, b_frames = get_frames(a_path, b_path)
+    print(f'a_frames={len(a_frames)}, a_frames={len(a_frames)}')
     left = 0
-    for a_tuple in a_list:
-        while left < len(b_list) and not is_coincident(a_tuple, b_list[left], max_time_diff) and a_after_b(a_tuple, b_list[left]):
+    for a_frame in a_frames:
+        # Increment the left pointer in b_frames until a coincident frame is found.
+        while left < len(b_frames) \
+                and a_after_b(a_frame, b_frames[left]) \
+                and is_coincident(a_frame, b_frames[left], max_time_diff):
             left += 1
         right = left
-        while right < len(b_list) and is_coincident(a_tuple, b_list[right], max_time_diff):
-            pairs.append((a_tuple, b_list[right]))
+        while right < len(b_frames) and is_coincident(a_frame, b_frames[right], max_time_diff):
+            frame_pair = a_frame, b_frames[right]
+            if frame_pair in pairs:
+                print(f'duplicate frame pair: \n\t{frame_pair[0]}\n\t{frame_pair[1]}')
+            pairs.append(frame_pair)
             right += 1
     return pairs
 
@@ -164,21 +165,26 @@ def plot_frame(fig, ax, frame):
 
 
 def plot_coincidence(a, b, max_time_diff):
-    fig, axs = plt.subplots(1, 2, figsize=(7, 4))
-    for ax, frame in zip(axs, [a,b]):
+    fig, axs = plt.subplots(1, 2, figsize=(12, 7))
+    for ax, frame in zip(axs, [a, b]):
         plot_frame(fig, ax, frame)
     style_fig(fig, fname_a, a[1]['mod_num'], fname_b, b[1]['mod_num'], max_time_diff)
-    fig.show()
+    plt.show()
+
 
 def main():
-    max_time_diff = 100
-    pairs = do_search(fpath_a, fpath_b, max_time_diff, 0)
-    for pair in pairs:
-        mod_a, mod_b = pair[0], pair[1]
-        #print(f'Module A: {mod_a[1]}\nModule B: {mod_b[1]}')
-        plot_coincidence(mod_a, mod_b, max_time_diff)
-    #plt.colorbar()
-    plt.show()
+    max_time_diff = 1_000_000_000
+    pairs = sorted(do_search(fpath_a, fpath_b, max_time_diff, 0))
+    if len(pairs) == 0:
+        print('\nNo coincident pairs found.')
+        sys.exit(0)
+    do_plot = input(f'\nPlot {len(pairs)} figures? (Y/N): ')
+    if do_plot.lower() == 'y':
+        for pair in pairs:
+            mod_a, mod_b = pair[0], pair[1]
+            #print(f'Module A: {mod_a[1]}\nModule B: {mod_b[1]}')
+            plot_coincidence(mod_a, mod_b, max_time_diff)
+
 
 if __name__ == '__main__':
     main()
