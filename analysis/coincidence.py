f8794cc45c72c113b34080c7d401e7fbd99d4af1
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2022-08-03 22:42:53 -0700

    coincidence.py: Refactor figure styling. Add pe threshold filtering. Add comments.

--- analysis/coincidence.py
+++ analysis/coincidence.py
@@ -20,7 +20,7 @@ import config_file
 
 # Default data
 DATA_IN_DIR = '/Users/nico/Downloads/720_ph_12pe'
-DATA_OUT_DIR = '/Users/nico/panoseti/data_figures/coincidence/2022_07_20_ph_12pe_100ns'
+DATA_OUT_DIR = '/Users/nico/panoseti/data_figures/coincidence/2022_07_20_100ns_1500pe'
 
 
 #fname_a = 'start_2022-07-20T06_44_48Z.dp_ph16.bpp_2.dome_0.module_1.seqno_0.pff' # astrograph 1
@@ -42,6 +42,10 @@ def get_module_file(module_ip_addr):
     mod_num = parsed['module']
 '''
 
+
+def get_max_pe(frame):
+    """Returns the maximum raw adc from the image in frame."""
+    return max(frame[2])
 
 def get_pkt_timestamp(frame):
     """Returns the pkt timestamp of frame."""
@@ -79,6 +83,7 @@ def a_after_b(a, b):
 def get_next_frame(file_obj, frame_num):
     # Get the next frame from module A
     j, img = None, None
+    print(f'Processed up to frame {frame_num:,}... ', end='')
     try:
         j = pff.read_json(file_obj)
         j = json.loads(j.encode())
@@ -90,10 +95,11 @@ def get_next_frame(file_obj, frame_num):
     if not j or not img:
         return None
     frame = (frame_num, j, img)
+    print('\r', end='')
     return frame
 
 
-def search(a_path, b_path, max_time_diff, threshold_pe):
+def search(a_path, b_path, max_time_diff, threshold_max_pe):
     """
     Identify all pairs of frames from the files a_path and b_path with timestamps that
     differ by no more than 100ns.
@@ -105,41 +111,45 @@ def search(a_path, b_path, max_time_diff, threshold_pe):
     b_deque = deque()
 
     def b_deque_right_append_next_frame(b_file_obj):
+        """Right append the next frame in b_file_obj to b_deque"""
         nonlocal b_frame_num
         b_frame = get_next_frame(b_file_obj, b_frame_num)
         if b_frame is not None:
             b_deque.append(b_frame)
             b_frame_num += 1
-
     with open(a_path, 'rb') as fa, open(b_path, 'rb') as fb:
         b_deque_right_append_next_frame(fb)
         while True:
-            print(f'Module A: processed up to frame {a_frame_num:,}... ', end='')
             # Get the next frame for module A and check if we've reached EOF.
             a_frame = get_next_frame(fa, a_frame_num)
             if a_frame is None:
                 break
-            # Left pop b_deque until a coincident frame is found.
-            while len(b_deque) > 0 \
-                    and a_after_b(a_frame, b_deque[0]) \
-                    and not is_coincident(a_frame, b_deque[0], max_time_diff):
-                b_deque.popleft()
-                # Right append frames if b_deque runs out of frames.
-                if len(b_deque) == 0:
-                    b_deque_right_append_next_frame(fb)
-            # Check frames that appear after b_deque[0] until a non-coincident frame is found.
-            right_index = 0
-            while right_index < len(b_deque) and is_coincident(a_frame, b_deque[right_index], max_time_diff):
-                # Each coincident pair of frames is added to the list pairs.
-                frame_pair = a_frame, b_deque[right_index]
-                if frame_pair in pairs:
-                    print(f'duplicate frame pair: \n\t{frame_pair[0]}\n\t{frame_pair[1]}')
-                pairs.append(frame_pair)
-                right_index += 1
-                if right_index >= len(b_deque):
-                    b_deque_right_append_next_frame(fb)
+            elif get_max_pe(a_frame) < threshold_max_pe:
+                a_frame_num += 1
+                continue
+            else:
+                # Left pop b_deque until a coincident frame is found.
+                while len(b_deque) > 0 and a_after_b(a_frame, b_deque[0]) \
+                        and not is_coincident(a_frame, b_deque[0], max_time_diff):
+                    b_deque.popleft()
+                    # Right append frames if b_deque runs out of frames.
+                    if len(b_deque) == 0:
+                        b_deque_right_append_next_frame(fb)
+                # Inspect every frame that appears after b_deque[0] until a non-coincident frame is found.
+                right_index = 0
+                while right_index < len(b_deque) and is_coincident(a_frame, b_deque[right_index], max_time_diff):
+                    b_frame = b_deque[right_index]
+                    if get_max_pe(b_frame) >= threshold_max_pe:
+                        # Each coincident pair of frames is added to the list pairs.
+                        frame_pair = a_frame, b_frame
+                        if frame_pair in pairs:
+                            print(f'duplicate frame pair: \n\t{frame_pair[0]}\n\t{frame_pair[1]}')
+                        pairs.append(frame_pair)
+                    right_index += 1
+                    if right_index >= len(b_deque):
+                        b_deque_right_append_next_frame(fb)
             a_frame_num += 1
-            print('\r', end='')
+            #print('\r', end='')
     print('Done!')
     return pairs
 
@@ -153,26 +163,33 @@ def get_image_2d(image_1d):
     return rect
 
 
-def style_fig(fig, fname_a, fname_b, max_time_diff, fig_num):
-    # Configure each figure
+def style_fig(fig, fig_num, fname_a, fname_b, max_time_diff, threshold_max_pe):
+    # Style each figure
     parsed_a, parsed_b = pff.parse_name(fname_a), pff.parse_name(fname_b)
-    title = f"Pulse Height Event from Module {parsed_a['module']} and Module {parsed_b['module']} within {max_time_diff:,} ns"
-    title += f"\nLeft: Dome: {parsed_a['dome']}, Module {parsed_a['module']}; Start: {parsed_a['start']}; Seq No: {parsed_a['seqno']}"
-    title += f"\nRight: Dome: {parsed_b['dome']}, Module {parsed_b['module']}; Start: {parsed_b['start']}; Seq No: {parsed_b['seqno']}"
+    title = "Pulse Height Event from Module {0} and Module {1} within {2:,} ns and max(pe) >= {3:,}".format(
+        parsed_a['module'], parsed_b['module'], max_time_diff, threshold_max_pe
+    )
+    title += "\nLeft: Dome: {0}, Module {1}; Start: {2}; Seq No: {3}".format(
+        parsed_a['dome'], parsed_a['module'], parsed_a['start'], parsed_a['seqno']
+    )
+    title += "\nRight: tDome: {0}, Module {1}; Start: {2}; Seq No: {3}".format(
+        parsed_b['dome'], parsed_b['module'], parsed_b['start'], parsed_b['seqno']
+    )
     fig.suptitle(title)
     fig.tight_layout()
     canvas = fig.canvas
     canvas.manager.set_window_title(f'Figure {fig_num:,}')
-    canvas.get_default_filename = \
-        lambda: f"start_{parsed_b['start']}.dome_{parsed_a['dome']}.module_{parsed_a['module']}.seqno_{parsed_a['seqno']}" \
-                + f"__dome_{parsed_b['dome']}.module_{parsed_b['module']}.seqno_{parsed_b['seqno']}" + \
-                f".fignum_{fig_num}.{canvas.get_default_filetype()}"
+    save_name = "fig-num_{0}.ns-diff_{1}.threshold-pe_{2}.left-module_{3}.right-module_{4}.{5}".format(
+        fig_num, max_time_diff, threshold_max_pe, fname_a[:-4], fname_b[:-4], canvas.get_default_filetype()
+    )
+    canvas.get_default_filename = lambda: save_name
 
 
 def style_ax(fig, ax, frame, plot):
+    # Style each plot.
     ax.set_box_aspect(1)
-    metadata_text = 'Mod {0}, Quabo {1}: pkt_num={2}, \npkt_utc={3}, pkt_nsec={4},\n tv_sec={5}, tv_usec={6}'.format(
-        frame[1]['mod_num'], frame[1]['quabo_num'], frame[1]['pkt_num'], frame[1]['pkt_utc'],
+    metadata_text = 'Mod {0}, Quabo {1}: frame#{2:,},\npkt_num={3}, pkt_utc={4}, pkt_nsec={5},\n tv_sec={6}, tv_usec={6}'.format(
+        frame[1]['mod_num'], frame[1]['quabo_num'], frame[0], frame[1]['pkt_num'], frame[1]['pkt_utc'],
         frame[1]['pkt_nsec'], frame[1]['tv_sec'], frame[1]['tv_usec'])
     ax.set_title(metadata_text)
     cbar = fig.colorbar(plot, ax=ax, fraction=0.035, pad=0.05)
@@ -181,34 +198,41 @@ def style_ax(fig, ax, frame, plot):
 
 
 def plot_frame(fig, ax, frame):
+    # Draw the 2d image of a frame.
     frame_img = get_image_2d(frame[2])
     plot = ax.pcolormesh(np.arange(16), np.arange(16), frame_img)
     style_ax(fig, ax, frame, plot)
 
 
-def plot_coincidence(a, b, max_time_diff, fig_num):
+def plot_coincidence(fig_num, a, b, max_time_diff, threshold_max_pe, save_fig=False):
     fig, axs = plt.subplots(1, 2, figsize=(15, 8))
     for ax, frame in zip(axs, [a, b]):
         plot_frame(fig, ax, frame)
-    style_fig(fig, fname_a, fname_b, max_time_diff, fig_num)
+    style_fig(fig, fig_num, fname_a, fname_b, max_time_diff, threshold_max_pe)
     #input(fig.canvas.get_default_filename())
-    #plt.savefig(f'{DATA_OUT_DIR}/{fig.canvas.get_default_filename()}')
-    plt.show()
+    if save_fig:
+        plt.savefig(f'{DATA_OUT_DIR}/{fig.canvas.get_default_filename()}')
+        plt.close(fig)
+    else:
+        plt.show()
+        plt.close(fig)
 
 
-def do_search():
-    max_time_diff = 0
-    pairs = sorted(search(fpath_a, fpath_b, max_time_diff, 0))
+def do_search(verbose=True):
+    max_time_diff = 100
+    # Only include images with a max pe value of at least the following:
+    threshold_max_pe = 1500
+    pairs = sorted(search(fpath_a, fpath_b, max_time_diff, threshold_max_pe))
     if len(pairs) == 0:
         print(f'No coincident frames found within {max_time_diff:,} ns of each other.')
         sys.exit(0)
     do_plot = input(f'Plot {len(pairs)} figures? (Y/N): ')
     if do_plot.lower() == 'y':
         for fig_num, pair in enumerate(pairs):
-            print(f'\nFigure {fig_num:,}:')
+            if verbose: print(f'\nFigure {fig_num:,}:')
             mod_a, mod_b = pair[0], pair[1]
-            print(f'Left module: {mod_a[1]}\nRight module : {mod_b[1]}')
-            plot_coincidence(mod_a, mod_b, max_time_diff, fig_num)
+            if verbose: print(f'Left module: {mod_a[1]}\nRight module : {mod_b[1]}')
+            plot_coincidence(fig_num, mod_a, mod_b, max_time_diff, threshold_max_pe, save_fig=False)
             fig_num += 1
 
 
