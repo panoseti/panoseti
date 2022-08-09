f246c2800076e1b8333cc9590f97abcb85579307
David Anderson <davea@berkeley.edu>
2022-08-09 15:52:05 -0800

    Web: added a Python version of the home page. I'm tempted to convert all web stuff from PHP to Python because 1) we wouldn't need separate PHP utilities, e.g. for parsing PFF names 2) PanoSETI developers wouldn't have to know PHP
    I installed the Apache interface to Python (WSGI),
    added/modified Apache config files, and got things working.
    
    However, there were lots of little irritations,
    and now I'm having second thoughts.
    For example: WSGI doesn't look at the mod times of Python scripts,
    so you have to restart Apache each time you change something.

--- util/pff.py
+++ util/pff.py
@@ -84,6 +84,8 @@ def parse_name(name):
         d[y[0]] = y[1]
     return d
 
+    print(time.strptime('2022-06-30T21:03:20Z', '%Y-%m-%dT%H:%M:%SZ'))
+
 # return the directory name for a run
 #
 def run_dir_name(obs_name, run_type):
---
+++ web/index.py
@@ -0,0 +1,52 @@
+import json
+import os, sys
+sys.path.insert(1, '/home/panosetigraph/web')
+from web_util import *
+import pff
+
+def body():
+    x = ''
+    x += '''
+        <h2>Graphical parameter logs</h2>
+        <p>
+        <a href=http://visigoth.ucolick.org:3000>View</a>
+    '''
+    x += '''
+        <h2>Observing runs</h2>
+        <p>
+    '''
+    x += table_start('table-striped')
+    x += table_header(['observatory', 'start', 'run type', 'click to view'])
+    runs = []
+    for run in os.listdir('/home/panosetigraph/web/data'):
+        if not pff.is_pff_dir(run):
+            continue
+        n = pff.parse_name(run)
+        runs.append([n['start'], run])
+    runs = sorted(runs, key=lambda x: x[0], reverse=True)
+    prev_day = ''
+    for run in runs:
+        name = run[1]
+        n = pff.parse_name(name)
+        start = run[0]
+        s = start.split('T')
+        day = s[0]
+        time = s[1]
+        if day != prev_day:
+            x += table_subheader(day)
+            prev_day = day
+        x += table_row([
+            n['obs'], time, n['runtype'],
+            '<a href=run.php?name=%s>View</a>'%name
+        ])
+    x += table_end();
+
+    return x
+
+print(body())
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-type','text/html')])
+    html = page_head('PanoSETI')+body()+page_tail()
+    html = bytes(html, encoding='utf-8')
+    return [html]
