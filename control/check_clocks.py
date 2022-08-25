d53a64b183c5ee9fb6903a70cba663d61f79c774
Wei Liu <liuwei_berkeley@berkeley.edu>
2022-08-25 15:05:24 -0700

    1. make some changes for the time sync checking script:  (1) remove 'panoseti_' from the filename and class name;  (2) change the filename from 'checksys' to 'check_clocks';  (3) add comments for the class(check_clocks) and all functions in this class;
    2. remove some unnecessaru files, such as flashuid, quabo_uids.json...
    
    3. add .gitignore, so that git won't care about the unnecessary files.

--- control/check_clocks.py
+++ control/check_clocks.py
@@ -1,5 +1,8 @@
 #! /usr/bin/python
 
+# 'check_clocks' is a class for checking time synchronization in PANOSETI system.
+# There are several functions in this class for getting time from GPS receiver, quabo and WRS.
+# 
 import time
 import serial
 from datetime import datetime, timezone
@@ -7,36 +10,39 @@ import socket
 import paramiko
 import struct
 import qstart
+import qstart, config_file
 
 # this is the offset time between tai and utc
 LEAP_SEC = 37
 # this is the offset time between gps and utc
 GPS_SEC  = 18
 # time difference between host time and device time
-TOLARANCE = 0.5
+TOLERANCE = 0.5
 
+# create ssh session for getting wrs time
+#
 def SSH_Init(wrs_ip):
     ssh=paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(wrs_ip,username='root',password='')
     return ssh
 
-class checksys(object):
+
+# The class is used for checking timing synchronization in PANOSETI system.
+#
+class check_clocks(object):
     def __init__(self, gps_port='/dev/ttyUSB0', wrs_ip='192.168.1.254', host_ip='192.168.1.100', port=60001):
         self.gps_port = gps_port
         self.host_ip = host_ip
         self.port = port
         self.wrs_ip = wrs_ip
-        # configure the serial connections (the parameters differs on the device you are connecting to)
         self.ser = 0
         self.ssh =  0
 
-    """
-    The code is for getting time from GPS Resceiver.
-    The GPS receiver should be connected to the host computer via a USB port,
-    which is shown as /dev/ttyUSBx.
-    """
-    def primary_timing_packet(self,data):
+    
+    # parse primary timing packets from GPS receiver
+    #
+    def _parse_primary_packet(self,data):
         # check the length of data
         if len(data) != 17:
             return
@@ -51,7 +57,11 @@ class checksys(object):
         # there is no nanosec info from GPS receiver, so nanosec value is set to 0 here
         lastTime = datetime(year, month, dayofMonth, hours, minutes, seconds, 0).replace(tzinfo=timezone.utc)
         return lastTime.timestamp() - GPS_SEC
-
+ 
+    
+    # Get time from GPS receiver.
+    # The information is from a serial port, which is defined in obs_config.json.
+    #
     def get_gps_time(self):
 
         self.ser = serial.Serial(
@@ -96,7 +106,7 @@ class checksys(object):
                 if data[0:1] == b'\x10':
                     id = data[1:3]
                     if id == b'\x8f\xab':
-                        gps_time = self.primary_timing_packet(data[2:dataSize-2])
+                        gps_time = self._parse_primary_packet(data[2:dataSize-2])
                         if(gps_time !=None):
                             recv_state = False
                 data = b''
@@ -106,9 +116,10 @@ class checksys(object):
         self.ser.close()
         return gps_time, t_host
 
-    """
-    The code is for getting time from the host computer and quabo.
-    """
+    
+    # Get time from a quabo.
+    # We just use 1 quabo for the test, and the IP address of the quabo is defined in obs_config.json.
+    #
     def get_quabo_time(self):
         BUFFERSIZE = 1024
         IP_PORT = (self.host_ip,self.port)
@@ -132,9 +143,11 @@ class checksys(object):
         
         return t_quabo, t_host
 
-    """
-    The code is for getting time from WRS.
-    """
+    
+    # Get time from WRS.
+    # The information is from a ssh session, which requires the IP address of the WRS.
+    # The IP address is defined in obs_config.json.
+    #
     def get_wrs_time(self):
         cmd0 = "/wr/bin/wr_date get"
 
@@ -150,27 +163,40 @@ class checksys(object):
         del(self.ssh,ssh_stdin, ssh_stdout, ssh_stderr)
         return wrs_time, t_host
 
+    
+    # Compare the GPS time with host computer time.
+    #
     def check_gps_time(self):
         t_gps, t_host = self.get_gps_time()
-        if(abs(t_gps - t_host) < TOLARANCE):
+        if(abs(t_gps - t_host) < TOLERANCE):
             return True
         else:
             return False
+
     
+    # Compare the quabo time with host computer time.
+    #  
     def check_quabo_time(self):
         t_quabo, t_host = self.get_quabo_time()
-        if(abs(t_quabo - t_host) < TOLARANCE):
+        if(abs(t_quabo - t_host) < TOLERANCE):
             return True
         else:
             return False
     
+    
+    # Compare the WRS time with host computer time.
+    #
     def check_wrs_time(self):
         t_wrs, t_host = self.get_wrs_time()
-        if(abs(t_wrs - t_host) < TOLARANCE):
+        if(abs(t_wrs - t_host) < TOLERANCE):
             return True
         else:
             return False
     
+    
+    # Check the time from GPS reciever, quabo and WRS.
+    # If all of the time is good, it means the system timing is synced.
+    #
     def check_time_sync(self):
         s0 = self.check_gps_time()
         s1 = self.check_quabo_time()
@@ -181,32 +207,40 @@ class checksys(object):
             return False
 
 if __name__ == '__main__':
-    gps_port = '/dev/ttyUSB0'
-    host_ip = '192.168.1.100'
-    port = 60001
-    wrs_ip = '192.168.1.254'
+    # get uart_port, wrs_ip from config file
+    obs_config = config_file.get_obs_config()
+    gps_port = obs_config['gps_port']
+    wrs_ip = obs_config['wr_ip_addr']
+
     print('===============================================================')
     print('Please make sure:')
     print('1. The dev name of the GPS receiver is'.ljust(46,' '),gps_port)
     print('2. The IP address of the host computer is'.ljust(46,' '),host_ip)
-    print('3. The port for quabo packets is'.ljust(46,' '),str(port))
-    print('4. The IP address of WRS is'.ljust(46,' '),wrs_ip)
+    print('3. The IP address of WRS is'.ljust(46,' '),wrs_ip)
     print('===============================================================')
     print('Time Checking Result(UTC TIME):')
-    cts = checksys(gps_port, wrs_ip, host_ip, port)
+    
+    cts = check_clocks(gps_port, wrs_ip)
+    
+    # get gps time from gps reciever
     t_gps,t_host = cts.get_gps_time()
     r0 = cts.check_gps_time()
-   
+    
+    # start image mode on 1 quabo
     qstart.qstart(True)
     t_quabo = 0
     t_host1 = 0
     r1 = True
+    # get quabo time
     t_quabo, t_host1 = cts.get_quabo_time()
     r1 = cts.check_quabo_time()
+    # stop image mode on the quabo
     qstart.qstart(False)
 
+    # get wrs time from white rabbit switch
     t_wrs, t_host2 = cts.get_wrs_time()
     r2 = cts.check_wrs_time()
+
     print('GPS Time'.ljust(20, ' '),':',t_gps)
     print('GPS Timestamp'.ljust(20,' '),':',t_host)
     print('Checking Result'.ljust(20,' '),':',r0,'\n')
@@ -216,4 +250,3 @@ if __name__ == '__main__':
     print('WRS Time'.ljust(20, ' '),':',t_wrs)
     print('WRS Timestamp'.ljust(20,' '),':',t_host2)
     print('Checking Result'.ljust(20,' '),':',r2,'\n')
-
