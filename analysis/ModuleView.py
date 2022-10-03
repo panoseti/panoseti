efe3d3623414db0aea0f6a25d299c4615812d28d
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2022-10-03 00:53:42 -0700

    ModuleView.py: add method to compute the RA-DEC coordinates of the center of a module's field of view at a given time.

--- analysis/ModuleView.py
+++ analysis/ModuleView.py
@@ -6,23 +6,31 @@ import astropy.time as t
 
 class ModuleView:
     """Simulate the FoV of a PanoSETI module on the celestial sphere."""
-    def __init__(self, module_id, start_time, obslat, obslon, obsalt, azimuth, elevation, pos_angle):
+    def __init__(self, module_id, start_time_utc, obslat, obslon, obsalt, azimuth, elevation, pos_angle):
         self.module_id = module_id
-        self.start_time = t.Time(start_time)
-        self.ra_dec_initial = self.get_ra_dec_initial(obslat, obslon, obsalt, azimuth, elevation)
-        self.pos_angle = pos_angle
-
-    def get_ra_dec_initial(self, obslat, obslon, obsalt, azimuth, elevation):
-        earth_loc = c.EarthLocation(
+        self.start_time_utc = start_time_utc
+        self.azimuth = azimuth * u.deg
+        self.elevation = elevation * u.deg
+        self.earth_loc = c.EarthLocation(
             lat=obslat*u.deg, lon=obslon*u.deg, height=obsalt*u.m, ellipsoid='WGS84'
         )
+        self.current_pos = self.initial_pos = self.get_fov_ra_dec_coords(start_time_utc)
+        self.pos_angle = pos_angle
+
+    def get_fov_ra_dec_coords(self, frame_utc):
+        """Return the RA-DEC coordinates of the center of the module's field of view at frame_utc."""
+        time = t.Time(frame_utc, format='unix')
         alt_alz_coords = c.SkyCoord(
-            az=azimuth*u.deg, alt=elevation*u.deg, location=earth_loc,
-            obstime=self.start_time, frame=c.AltAz
+            az=self.azimuth, alt=self.elevation, location=self.earth_loc,
+            obstime=time, frame=c.AltAz
         )
         return alt_alz_coords.transform_to('icrs')
 
 
-
-m = ModuleView(100, '2022-07-30 23:00:00', 10.3, 44.2, 234, 77, 77, 77)
-print(m.ra_dec_initial)
+m1 = ModuleView(42, 1656443180, 10.3, 44.2, 234, 77, 77, 77)
+print(m1.initial_pos)
+m2 = ModuleView(42, 1664776735, 10.3, 44.2, 234, 77, 77, 77)
+print(m2.initial_pos)
+m3 = ModuleView(42, 1664782234, 10.3, 44.2, 234, 77, 77, 77)
+print(m3.initial_pos)
+print(m3.current_pos)
