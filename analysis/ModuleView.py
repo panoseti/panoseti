Bug fixes. Use the Bresenham algorithm to compute the rasterized pixel FoVs after rotation.

analysis/ModuleView.py
@@ -18,7 +18,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import rotate
 
-from birdie_injection_utils import ra_dec_to_sky_array_indices, graph_sky_array
+from birdie_injection_utils import ra_dec_to_sky_array_indices, graph_sky_array, line
 import sky_band
 from sky_band import get_module_pixel_corner_coord_ftn
 
@@ -104,6 +104,7 @@ class ModuleView:
         self.center_ra = (self.center_ra + dt * 360 / (24 * 60 * 60)) % 360
         self.current_utc = frame_utc
         self.get_pixel_corner_coord = self.get_module_pixel_corner_coord_ftn(self.center_ra, self.center_dec)
+        print(f'center_ra, center_dec = {self.center_ra, self.center_dec}')
 
     def simulate_one_pixel_fov(self, px, py, sky_array, bounding_box, draw_sky_band):
         """Sum the intensities in each element of sky_array visible by pixel (px, py) and return a counter value
@@ -114,30 +115,46 @@ class ModuleView:
 
         left_index, high_index = ra_dec_to_sky_array_indices(left_ra, high_dec, bounding_box)
         right_index, low_index = ra_dec_to_sky_array_indices(right_ra, low_dec, bounding_box)
-        # RA coordinates may wrap around if larger than 24hrs.
-        """
-        if left_index > right_index:
-            if draw_sky_band:
-                self.sky_band[left_index:, low_index:high_index + 1] += 5
-                self.sky_band[:right_index + 1, low_index:high_index + 1] += 5
-            left = sky_array[left_index:, low_index:high_index + 1]
-            right = sky_array[:right_index + 1, low_index:high_index + 1]
-            total_intensity = math.floor((left + right).sum())
-        else:
-        """
+
         if draw_sky_band:
             self.sky_band[left_index:right_index + 1, low_index:high_index + 1] += 5
         total_intensity = math.floor(sky_array[left_index:right_index + 1, low_index:high_index + 1].sum())
         # Set pixel value
+        self.pixel_convex_raster(px, py, bounding_box)
         if total_intensity > self.max_pixel_counter_value:
             total_intensity = self.max_pixel_counter_value
         self.simulated_img_arr[px * self.pixels_per_side + py] = total_intensity
 
+    def pixel_convex_raster(self, px, py, bounding_box):
+        # Get sky_array indices for the corners of detector (px, py)
+        indices = [-1] * 4
+        for row in range(2):
+            for col in range(2):
+                x, y = self.get_pixel_corner_coord(px, py, row, col)
+                indices[2*row + col] = ra_dec_to_sky_array_indices(x, y, bounding_box)
+        #min_x, max_x = min(indices, key=lambda p: p[0]), max(indices, key=lambda p: p[0])
+        min_y, max_y = min(indices, key=lambda p: p[1])[1], max(indices, key=lambda p: p[1])[1]
+        #print(indices)
+        pts = {y: [float('inf'), float('-inf')] for y in range(min_y, max_y + 1)}
+        corners = [0, 1, 3, 2, 0]
+        for i in range(4):
+            x1, y1 = indices[corners[i]]
+            x0, y0 = indices[corners[i + 1]]
+            line(x0, y0, x1, y1, pts)
+
+        #print(f'pts={pts}')
+        for y in pts:
+            for x in range(pts[y][0], pts[y][1] + 1):
+                self.sky_band[x, y] = 10000
+
     def simulate_all_pixel_fovs(self, sky_array, bounding_box, draw_sky_band=False):
         """Simulate every pixel FoV in this module, resulting in a simulated 32x32 image array
         containing only birdies."""
+        print(bounding_box)
         if self.sky_band is None:
             self.sky_band = np.copy(sky_array)
         for i in range(self.pixels_per_side):
             for j in range(self.pixels_per_side):
                 self.simulate_one_pixel_fov(i, j, sky_array, bounding_box, draw_sky_band)
+        self.plot_sky_band()
+        input(f'i, j = {i, j}')

analysis/birdie_injection_utils.py
@@ -65,9 +65,9 @@ def ra_dec_to_sky_array_indices(ra, dec, bounding_box):
     return ra_index, dec_index
 
 
-def get_coord_bounding_box(ra_center, dec_center, r=2, pixel_scale=0.31, pixels_per_side=32):
+def get_coord_bounding_box(ra_center, dec_center, r=1.5, pixel_scale=0.31, pixels_per_side=32):
     """Return the ra-dec coordinates of the simulation bounding box centered at ra_center, dec_center.
-    r is the ratio of interval length to the module's fov width."""
+    """
     interval_radius = pixel_scale * pixels_per_side * (r / 2)
     # Interval of ra coordinates given ra_center, the center of the interval,
     ra_interval = ra_center - interval_radius, ra_center + interval_radius
@@ -76,18 +76,19 @@ def get_coord_bounding_box(ra_center, dec_center, r=2, pixel_scale=0.31, pixels_
     return ra_interval, dec_interval
 
 
-def init_sky_array_constants(elem_per_deg, r):
+def init_sky_array_constants(elem_per_deg):
+    """r is the ratio of interval length to the module's fov width."""
     global ra_length, ra_size, dec_length, dec_size
-    bounding_box = get_coord_bounding_box(0, 0, r)
+    bounding_box = get_coord_bounding_box(0, 0)
     ra_length = bounding_box[0][1] - bounding_box[0][0]
     dec_length = bounding_box[1][1] - bounding_box[1][0]
     ra_size = round(elem_per_deg * ra_length)
     dec_size = round(elem_per_deg * dec_length)
 
 
-def get_sky_image_array(elem_per_deg, r, verbose=False):
+def get_sky_image_array(elem_per_deg, verbose=False):
     """Returns a 2D array with shape (num_ra, num_dec)."""
-    init_sky_array_constants(elem_per_deg, r)
+    init_sky_array_constants(elem_per_deg)
     # 1st dim: RA coords, 2nd dim: DEC coords (both in degrees)
     array_shape = ra_size, dec_size
     if verbose:
@@ -112,7 +113,7 @@ def graph_sky_array(sky_array):
     ra_labels_in_hrs = 24 * ra_labels_in_deg / 360
     ax.set_xticks(
         np.linspace(0, sky_array.shape[0] - 1, 7),
-        ra_labels_in_hrs.round(3)
+        ra_labels_in_deg.round(3)
     )
     # Add DEC tick marks
     ax.set_yticks(
@@ -153,4 +154,29 @@ def dec_to_degrees(dec):
     abs_degrees = abs(dec[0]) + (dec[1] / 60) + (dec[2] / 3600)
     sign = dec[0] / abs(dec[0])
     return sign * abs_degrees
+
+def line(x0, y0, x1, y1, pts):
+    """"Bresenham's line algorithm:
+    https://circuitcellar.com/resources/bresenhams-algorithm
+    """
+    dx = x1 - x0 if x1 >= x0 else x0 - x1
+    dy = y0 - y1 if y1 >= y0 else y1 - y0
+    sx = 1 if x0 < x1 else -1
+    sy = 1 if y1 >= y0 else -1
+    err = dx + dy
+    x = x0
+    y = y0
+    #print(f'dx, dy, sx, sy, err, x, y={dx, dy, sx, sy, err, x, y}')
+    #print(f'x0, y0, x1, y1={x0, y0, x1, y1}')
+    while True:
+        pts[y][0], pts[y][1] = min(pts[y][0], x), max(pts[y][1], x)
+        if x == x1 and y == y1:
+            break
+        e2 = 2 * err
+        if e2 >= dy: # step x
+            err += dy
+            x += sx
+        if e2 <= dx: # step y
+            err += dx
+            y += sy
 