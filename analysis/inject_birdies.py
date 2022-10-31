ac0cff572a98f7c8b01475e09d464564b2e5249a
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2022-10-31 00:23:24 -0700

    inject_birdies.py: Add code to create birdie injection directory and copy image mode files.

--- analysis/inject_birdies.py
+++ analysis/inject_birdies.py
@@ -20,12 +20,13 @@ import math
 import time
 import sys
 import os
+import shutil
 import numpy as np
 import cProfile
 
-import analysis_util
 import birdie_utils
 import birdie_simulation as birdie_sim
+import analysis_util
 
 sys.path.append('../util')
 import pff
@@ -53,28 +54,37 @@ def test_simulation():
     cProfile.runctx(f, globals(), locals(), sort='tottime')
 
 
-def do_file(data_dir, run, analysis_dir, fin_name, params):
-    print('processing file ', fin_name)
+def do_file(data_dir, run, birdie_run, fin_name, sequence_num, params):
+    """Inject birdies into a single file."""
     file_attrs = pff.parse_name(fin_name)
-    module_id = file_attrs['module']
-    module_dir = analysis_util.make_dir('%s/module_%s_with_birdies'%(analysis_dir, module_id))
     birdie_config = birdie_utils.get_birdie_config('birdie_config.json')
 
     # Get start and end utc timestamps
     start_iso = pff.parse_name(run)['start']  # use the timestamp from the run directory name.
-    run_complete_path = f'{data_dir}/{run}/run_complete'
-    with open(run_complete_path) as f:
+    with open(f'{data_dir}/{run}/run_complete') as f:
         end_iso = f.readline()  # use the timestamp in "data/$run/run_complete"
+
     # Get timing info
     start_utc = birdie_utils.iso_to_utc(start_iso)
     end_utc = birdie_utils.iso_to_utc(end_iso)
     integration_time = birdie_utils.get_integration_time(data_dir, run)
     print(f'start_utc={start_utc}, end_utc={end_utc}, integration_time={integration_time} us')
+    dt = end_utc-start_utc
+    print(f'recording_time = {int(dt // 60)}:{int(dt%60)}')
 
+    fout_name = fin_name.replace('.pff', '') + f'.birdie_{sequence_num}.pff'
+    fout_path = f'{data_dir}/{birdie_run}/{fout_name}'
+    fin_path = os.path.abspath(f'{data_dir}/{run}/{fin_name}')
+
+    #print(f"fin= {fin_path},\nfout={fout_path}")
     # Do simulation
-    #input(f'{module_dir}/birdie-injection.{fin_name}')
-    with open(f'{data_dir}/{run}/{fin_name}', 'rb') as fin:
-        with open(f'{module_dir}/birdie-injection.{fin_name}', 'w+b') as fout:
+    with open(fin_path, 'rb') as fin:
+        with open(fout_path, 'w+b') as fout:
+            # Create a copy of the file fin_name for birdie injection.
+            print(f'Copying:\n\tFrom:\t{fin_name}\n\tTo:\t\t{fout_name}')
+            shutil.copy(fin_path, fout_path)
+
+            return
             birdie_sim.do_simulation(
                 start_utc, end_utc,
                 birdie_config,
@@ -87,31 +97,47 @@ def do_file(data_dir, run, analysis_dir, fin_name, params):
             )
 
 
-def do_run(data_dir, run, params, username):
-    analysis_dir = analysis_util.make_dir('birdie_injection_test')#analysis_util.make_analysis_dir('birdie_injection', run)
-    print('processing run', run)
+def do_run(data_dir, run, fin_name, params):
+    """Run birdie injection on a real observing run."""
+    if not pff.is_pff_dir(run):
+        print(f'"{run}" is not a pff directory')
+        return
+    print('Injecting birdies into run ', run)
+    # Get sequence number
+    run_name_without_pffd = run.replace('.pffd', '')
+    sequence_num = 0
+    for f in os.listdir(f'{data_dir}'):
+        if run_name_without_pffd in f.replace('.pffd', ''):
+            run_attrs = pff.parse_name(f)
+            if 'birdie' in run_attrs:
+                sequence_num += 1
+    #print(f'sequence_num = {sequence_num}')
+    # Create directory for run + birdie data
+    birdie_run = run_name_without_pffd + f'.birdie_{sequence_num}.pffd'
+    analysis_util.make_dir(f'{data_dir}/{birdie_run}')
     for f in os.listdir(f'{data_dir}/{run}'):
         if not pff.is_pff_file(f):
             continue
-        if pff.pff_file_type(f) != 'img16':
+        if pff.pff_file_type(f) not in ('img16', 'img8'):
             continue
-        #input(pff.parse_name(run))
-        do_file(data_dir, run, analysis_dir, f, params)
-    analysis_util.write_summary(analysis_dir, params, username)
+        do_file(data_dir, run, birdie_run, fin_name, sequence_num, params)
+    print(f'Finished injecting birdies.')
 
 
-DATA_DIR = '/Users/nico/Downloads/test_data/data'
-fname = 'start_2022-05-11T23/39/15Z.dp_1.bpp_2.dome_0.module_1.seqno_1.pff'
+
+DATA_DIR = '/Users/nico/Downloads/test_data'
+RUN = 'obs_Lick.start_2022-10-13T00:08:12Z.runtype_eng.pffd'
+fname = 'start_2022-10-13T00:08:28Z.dp_img8.bpp_1.module_1.seqno_0.pff'
 
 
 def main():
-    #analysis_dir = analysis_util.make_analysis_dir('birdie_injection', run)
     params = {
         'seconds': 1
     }
-    #do_run(DATA_DIR, 'obs_Lick.start_2022-05-11T23:38:29Z.runtype_eng.pffd', params, 'nico')
+    do_run(DATA_DIR, RUN, fname, params)
     #exec('test_simulation()')
-    test_simulation()
+    #test_simulation()
+
 
 if __name__ == '__main__':
     print("RUNNING")
