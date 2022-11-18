bc42e3d4b7ce2ed7eb26c6576460abd049518f50
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2022-11-17 19:56:30 -0800

    ph_coincidence.py: Change module pair specification. Ensure user-supplied module pairs are valid.

--- analysis/ph_coincidence.py
+++ analysis/ph_coincidence.py
@@ -11,6 +11,7 @@ import os
 import sys
 import getpass
 import itertools
+import re
 
 from search_ph import do_coincidence_search
 from analysis_util import make_dir, make_analysis_dir, write_summary, ANALYSIS_TYPE_PULSE_HEIGHT_COINCIDENCE
@@ -20,28 +21,6 @@ import pff
 import config_file
 
 # Control
-
-#a_fname = 'start_2022-06-28T19_06_14Z.dp_ph16.bpp_2.dome_0.module_1.seqno_0.pff'
-#b_fname = 'start_2022-06-28T19_06_14Z.dp_ph16.bpp_2.dome_0.module_1.seqno_0.pff'
-#a_fname = 'start_2022-06-28T19_06_14Z.dp_ph16.bpp_2.dome_0.module_254.seqno_0.pff'
-
-# July 19
-#a_fname = 'start_2022-07-20T06_44_48Z.dp_ph16.bpp_2.dome_0.module_1.seqno_0.pff' # astrograph 1
-#a_fname = 'start_2022-07-20T06_44_48Z.dp_ph16.bpp_2.dome_0.module_254.seqno_0.pff' # astrograph 2
-#b_fname = 'start_2022-07-20T06_44_48Z.dp_ph16.bpp_2.dome_0.module_3.seqno_0.pff' # nexdome
-
-# July 20
-#a_fname = 'start_2022-07-21T06_03_03Z.dp_ph16.bpp_2.dome_0.module_1.seqno_0.pff' # astrograph 1
-#b_fname = 'start_2022-07-21T06_03_03Z.dp_ph16.bpp_2.dome_0.module_254.seqno_0.pff' # astrograph 2
-#b_fname = 'start_2022-07-21T06_03_03Z.dp_ph16.bpp_2.dome_0.module_3.seqno_0.pff' # nexdome
-
-
-def check_pair(module_to_process, module_a, module_b):
-    """Return True only if:
-        - module_a and module_b are in different domes.
-    """
-    in_different_domes = module_to_process[module_a]['dome'] != module_to_process[module_b]['dome']
-    return in_different_domes
 
 
 def do_pair(run_path, analysis_dir, params, obs_config, modules_to_process, bytes_per_pixel, module_a, module_b):
@@ -72,14 +51,33 @@ def do_pair(run_path, analysis_dir, params, obs_config, modules_to_process, byte
     )
 
 
+def check_all_module_pairs(available_modules, module_pairs_to_process):
+    """Return True only if:
+        - module_a and module_b have valid ph files in the specified run directory.
+        - module_a and module_b are in different domes.
+    """
+    no_ph_file_error_msg = 'Module {0} does not have a valid ph file in the specified run directory.'
+    not_in_diff_domes_error_msg = "Modules pairs must contain modules from different domes. " \
+                                  "Modules {0} and {1} are in the same dome."
+    for module_a, module_b in module_pairs_to_process:
+        # Valid ph files?
+        if module_a not in available_modules:
+            raise Warning(no_ph_file_error_msg.format(module_a))
+        elif module_b not in available_modules:
+            raise Warning(no_ph_file_error_msg.format(module_b))
+        # In different domes?
+        if available_modules[module_a]['dome'] == available_modules[module_b]['dome']:
+            raise Warning(not_in_diff_domes_error_msg.format(module_a, module_b))
+    return True
+
+
 def do_run(vol, run, params, username):
-    analysis_dir = make_analysis_dir(ANALYSIS_TYPE_PULSE_HEIGHT_COINCIDENCE, vol, run)
     run_path = f'{vol}/data/{run}'
     obs_config = config_file.get_obs_config(dir='../control')
     bytes_per_pixel = None
 
     # Get filepaths to each module specified by the user.
-    modules_to_process = dict()
+    available_modules = dict()
     for f in os.listdir(run_path):
         if not pff.is_pff_file(f): continue
         t = pff.pff_file_type(f)
@@ -98,42 +96,38 @@ def do_run(vol, run, params, username):
             file_attrs = pff.parse_name(f)
             module = int(file_attrs['module'])
             dome = int(file_attrs['dome'])
-            if module in modules_to_process:
+            if module in available_modules:
                 raise Warning(f'Expected exactly one ph file for module {module} but found more than one.')
-            modules_to_process[module] = dict()
-            if params['modules'] == 'all_modules':
-                modules_to_process[module]['fname'] = f
-                modules_to_process[module]['dome'] = dome
-            else:
-                if module in params['modules']:
-                    modules_to_process[module]['fname'] = f
-                    modules_to_process[module]['dome'] = dome
-    if modules_to_process:
-        # Process all distinct pairs of modules in modules_to_process
-        module_pairs = list(itertools.combinations(modules_to_process.keys(), 2))
-        for module_a, module_b in module_pairs:
-            if check_pair(modules_to_process, module_a, module_b):
-                do_pair(
-                    run_path,
-                    analysis_dir,
-                    params,
-                    obs_config,
-                    modules_to_process,
-                    bytes_per_pixel,
-                    module_a,
-                    module_b
-                )
-            else:
-                if params['verbose']:
-                    print(f"Modules {module_a} and {module_b} are in the same dome. Skipping this pair.")
+            available_modules[module] = {
+                'fname': f, 'dome': dome
+            }
+    if available_modules:
+        if params['modules'] == 'all_modules':
+            # Process all distinct pairs of modules in modules_to_process
+            module_pairs_to_process = list(itertools.combinations(available_modules.keys(), 2))
+        else:
+            module_pairs_to_process = params['modules']
+        check_all_module_pairs(available_modules, module_pairs_to_process)
+        analysis_dir = make_analysis_dir(ANALYSIS_TYPE_PULSE_HEIGHT_COINCIDENCE, vol, run)
+        for module_a, module_b in module_pairs_to_process:
+            do_pair(
+                run_path,
+                analysis_dir,
+                params,
+                obs_config,
+                available_modules,
+                bytes_per_pixel,
+                module_a,
+                module_b
+            )
+        write_summary(analysis_dir, params, username)
     else:
         print('No usable ph files found.')
-    write_summary(analysis_dir, params, username)
 
 
 def main():
     params = {
-        'modules': [1, 3],
+        'modules': [],
         'max_time_diff': 500,
         'threshold_max_adc': 0,
         'max_group_time_diff': 300,
@@ -148,21 +142,38 @@ def main():
     while i < len(argv):
         option = argv[i].replace('--', '', 1)
         if option in params:
-            i += 1
-            val = argv[i]
-            if option == 'modules' and val != 'all_modules':
-                try:
-                    val = list(map(int, val.split(',')))
-                except Exception as e:
-                    msg = f'Expected either "all_modules" or a comma separated ' \
-                          f'list of numbers after --module, not the given input "{val}"'
-                    print(msg)
-                    raise e
-            elif option == 'verbose' or option == 'save_fig':
-                val = val.lower() == 'true'
+            if option == 'modules':
+                # Since --modules receives user-input, we have to ensure the input is valid.
+                if argv[i + 1] == 'all_modules':
+                    if len(params['modules']) == 0:
+                        params['modules'] = argv[i + 1]
+                    else:
+                        raise Warning(f'Cannot supply both all_modules and module pairs with --modules.')
+                else:
+                    module_pair_pattern = re.compile("^(\d+),(\d+)$")
+                    while True:
+                        i += 1
+                        if i >= len(argv):
+                            break
+                        match = module_pair_pattern.match(argv[i])
+                        if '--' in argv[i]:
+                            i -= 1
+                            break
+                        elif match is not None:
+                            module_pair = int(match.group(1)), int(match.group(2))
+                            params['modules'].append(module_pair)
+                        elif argv[i] == 'all_modules':
+                            raise Warning(f'Cannot supply both all_modules and module pairs with --modules.')
+                        else:
+                            raise Warning('After --module, Expected either "all_modules" or a list of space-separated,'
+                                          'comma-separated module number pairs (e.g. "1,3 254,3"), '
+                                          f'not the given input: "{argv[i]}"')
+            elif option in ('verbose', 'save_fig'):
+                i += 1
+                params[option] = argv[i].lower() == 'true'
             else:
-                val = float(val)
-            params[option] = val
+                i += 1
+                params[option] = float(argv[i])
         elif option == 'run':
             i += 1
             run = argv[i]
@@ -173,15 +184,18 @@ def main():
             i += 1
             username = argv[i]
         else:
-            raise Warning(f'unrecognized input: "--{option} {argv[i]}". Options have the form: "--[option]".')
+            raise Warning(f'unrecognized input: "{option}". Options have the form: "--[option]".')
         i += 1
 
     if not run:
-        raise Exception('no run specified')
+        raise Warning('no run specified')
     if not vol:
-        raise Exception('no volume specified')
+        raise Warning('no volume specified')
     if not username:
         username = getpass.getuser()
+    if not params['modules']:
+        raise Warning('no modules specified')
+
 
     do_run(vol, run, params, username)
 
