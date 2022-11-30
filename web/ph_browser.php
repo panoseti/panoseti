195026c28cd4054a8fe8d1e5b15776f6dfc62514
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2022-11-30 14:06:50 -0800

    Fix bugs in event browser.

--- web/ph_browser.php
+++ web/ph_browser.php
@@ -13,6 +13,7 @@ function get_num_events($vol, $run, $analysis_dir, $module_pair_dir) {
     foreach(scandir($dirpath) as $event) {
         if (preg_match($event_pattern, $event)) {
             $n += 1;
+        }
     }
     return $n;
 }
@@ -20,10 +21,10 @@ function get_num_events($vol, $run, $analysis_dir, $module_pair_dir) {
 function arrows_str($vol, $run, $analysis_dir, $module_pair_dir, $event, $num_events) {
     $url = "ph_browser.php?vol=$vol&run=$run&analysis_dir=$analysis_dir&module_pair_dir=$module_pair_dir&event=";
     return sprintf(
-        <a class="btn btn-sm btn-primary" href=%s%d><< event</a>
-        <a class="btn btn-sm btn-primary" href=%s%d> event >></a>
+        '<a class="btn btn-sm btn-primary" href=%s%d><< event</a>
+        <a class="btn btn-sm btn-primary" href=%s%d> event >></a>',
         $url, ($event - 1) % $num_events,
-        $url, ($event + 1) % $num_events,
+        $url, ($event + 1) % $num_events
     );
 }
 
@@ -35,17 +36,22 @@ function show_event($event_path, $arrows) {
 }
 
 
-function main($vol, $run, $analysis_dir, $module_pair_dir, $event) {
-page_head("Pulse-Height Coincidence");
+function main($vol, $run, $analysis_dir, $module_pair_dir, $module_pair, $event) {
+    page_head("Pulse-Height Coincidence");
     echo "<p>Run: <a href=run.php?vol=$vol&name=$run>$run</a>\n";
-    echo "<p>Modules: $module_pair_dir\n";
+
+    echo "<p>Module pair: $module_pair";
     $num_events = get_num_events($vol, $run, $analysis_dir, $module_pair_dir);
-    echo "<p>Event: $event / $num_events\n";
+    echo sprintf(
+        '<p>Event: %d / %d',
+        $event + 1, $num_events
+    );
     $event_path = "$vol/analysis/$run/ph_coincidence/$analysis_dir/$module_pair_dir/event_$event.png";
     $as = arrows_str($vol, $run, $analysis_dir, $module_pair_dir, $event, $num_events);
     show_event($event_path, $as);
     page_tail();
 }
+
 
 $run = get_str("run");
 $vol = get_str("vol");
@@ -55,8 +61,9 @@ $analysis_dir = get_str("analysis_dir");
 check_filename($analysis_dir);
 $module_pair_dir = get_str("module_pair_dir");
 check_filename($module_event_dir);
+$module_pair = get_str("module_pair")
 $event = get_int("event");
 
-main($vol, $run, $analysis_dir, $module_pair_dir, $event);
+main($vol, $run, $analysis_dir, $module_pair_dir, $module_pair, $event);
 
 ?>
