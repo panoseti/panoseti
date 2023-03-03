2727234c5f987719842d2c0f92b7226b49543c98
Wei Liu <liuwei_berkeley@berkeley.edu>
2023-03-02 16:22:58 -0800

    update a little bit on sw_info.py.

--- control/sw_info.py
+++ control/sw_info.py
@@ -8,7 +8,10 @@ def get_sw_info():
     author = repo.head.commit.author.name
     branch = repo.active_branch.name
     commit_date = repo.head.commit.committed_datetime.strftime("%Y-%m-%d %H:%M:%S")
-    sw_info={'commit':commit,'author':author,'branch':branch,'commit_date':commit_date}
+    sw_info={'commit':commit,\
+             'author':author,\
+             'branch':branch,
+             'commit_date':commit_date }
     with open('sw_info.json','w') as f:
         json.dump(sw_info, f, indent=4)
 
