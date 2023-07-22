a54949cdfcaa509d9a20688f64a5af5c6adf1755
Wei Liu <liuwei_berkeley@berkeley.edu>
2023-07-21 17:09:48 -0700

    save quabo_config_*.json to data directory.

--- control/start.py
+++ control/start.py
@@ -153,7 +153,11 @@ def make_run_dirs(run_name, daq_config):
     # copy config files to run dir on this node
     local_data_dir = daq_config['head_node_data_dir']
     for f in config_file.config_file_names:
-        shutil.copyfile(f, '%s/%s'%(run_dir, f))
+        # TODO: not sure why using 'try' works here
+        try:
+            shutil.copyfile(f, '%s/%s'%(run_dir, f))
+        except:
+            pass
 
     # make module and run directories on DAQ nodes
     #
