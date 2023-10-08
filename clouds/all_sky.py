4b7fac9bdb2642f5c1b0295564c868b9f86ac8cd
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2023-10-07 23:30:42 -0700

    Add crop_panofov.py, a routine that extracts the panoseti fov from lick all-sky camera images.

--- clouds/all_sky.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-import matlab.engine
-eng = matlab.engine.start_matlab()
-eng.cd(r'all_sky_matlab', nargout=0)
-webcrep = "SC_imgs/"
-dateproc = "20230801"
-eng.panoplots(webcrep, 1.0, dateproc)
-eng.quit()
