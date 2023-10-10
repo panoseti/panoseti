4e2666ccce33b2815b4e5dd625d93157bfdd8b58
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2023-10-10 15:59:54 -0700

    Refactor sky cam preprocessing code and jupyter notebook.

--- clouds/preprocess_skycam.py
+++ clouds/preprocess_skycam.py
@@ -14,29 +14,37 @@ import os
 
 from PIL import Image
 
+
+def get_img_dirs(skycam_dir):
+    """Return """
+    original_skycam_dir = f'{skycam_dir}/original'
+    cropped_out_dir = f'{skycam_dir}/cropped'
+    pfov_out_dir = f'{skycam_dir}/pfov'
+    return original_skycam_dir, cropped_out_dir, pfov_out_dir
+
+
 def init_dirs(skycam_dir):
-    out_dir = f'processed_{skycam_dir}'
-    cropped_img_out_dir = f'{out_dir}/cropped'
-    pfov_out_dir = f'{out_dir}/pfov'
-    for dir_name in [out_dir, cropped_img_out_dir, pfov_out_dir]:
+    """Initialize pre-processing directories."""
+    cropped_out_dir, pfov_out_dir = get_img_dirs(skycam_dir)[1:]
+    for dir_name in [skycam_dir, cropped_out_dir, pfov_out_dir]:
         os.makedirs(dir_name, exist_ok=True)
-    return out_dir, cropped_img_out_dir, pfov_out_dir
 
 
 def get_corners():
-    # Get pixel corner data created by panofovlickwebc.m
+    """ Read the most recent pixel corner data created by panofovlickwebc.m"""
+    # TODO: search for the last astrometry calibration in the file.
     with open(pixel_data_file, 'r') as fp:
         pixel_data = json.load(fp)
         x_corners = np.round(pixel_data['SC2']['astrometry_entries'][0]['x_corners'])
         y_corners = np.round(pixel_data['SC2']['astrometry_entries'][0]['y_corners'])
         # Stack coords into an array of 2D points
-        # Points should be in the following order: in top left, top right, bottom right, bottom left
-        corners = np.vstack((x_corners, y_corners)).astype(np.int64).T
-        #print(corners.shape)
-        return corners
+        corners_4x2 = np.vstack((y_corners, x_corners)).astype(np.int64).T
+        corners_4x1x2 = np.expand_dims(corners_4x2, axis=1)     # Shape for CV2 coord arrays
+        return corners_4x1x2
 
 
-def crop_img(img, corners, cropped_fname):
+def crop_img(img, corners, cropped_fpath):
+    """Extract the panoseti FoV from a Lick all-sky camera image."""
     # Transformation code from https://jdhao.github.io/2019/02/23/crop_rotated_rectangle_opencv/
     # Find minimum bounding rectangle
     rect = cv2.minAreaRect(corners)
@@ -57,6 +65,7 @@ def crop_img(img, corners, cropped_fname):
                         [width-1, 0],
                         [width-1, height-1]], dtype="float32")
 
+    dst_pts = np.roll(dst_pts, shift=-1, axis=0)
     # the perspective transformation matrix
     M = cv2.getPerspectiveTransform(src_pts, dst_pts)
 
@@ -64,48 +73,57 @@ def crop_img(img, corners, cropped_fname):
     try:
         warped = cv2.warpPerspective(img, M, (width, height))
         # save cropped img to file
-
-        cv2.imwrite(cropped_fname, warped)
+        cv2.imwrite(cropped_fpath, warped)
     except Exception:
-        print(img_fname)
+        print(cropped_fpath)
         print(img)
         print("shape of corners: {}".format(corners.shape))
         print("rect: {}".format(rect))
         print("bounding box: {}".format(box))
 
 
-def plot_pfov(img, corners, pfov_fname):
+def plot_pfov(skycam_img, corners, pfov_fpath):
+    """Plot the panoseti module FoV on the sky camera image"""
     # Blue color in BGR
     color = (255, 0, 0)
-    poly_img = cv2.polylines(img.astype(np.int32), corners, isClosed=True, color=color, thickness=10)
-    # Displaying the image
-    cv2.imwrite(pfov_fname, poly_img)
-    # poly = Polygon(corners, closed=False, fill=True)
-    # ax = plt.imshow(img)
-    # #fig, ax = plt.subplots()
-    # ax.add_patch(poly)
-    #
-    # #x, y = poly.exterior.
-    # #plt.plot(x, y, c="red")
-    # plt.show()
+    #skycam_img = skycam_img.reshape((-1, 1, 2))
+    #print(skycam_img)
+    poly_img = cv2.polylines(skycam_img,
+                             [corners],
+                             isClosed=True,
+                             color=color,
+                             thickness=2)
+    # Save the image
+    cv2.imwrite(pfov_fpath, poly_img)
+
+def get_img_path(original_fname, img_type, skycam_dir):
+    original_sc2_dir, cropped_out_dir, pfov_out_dir = get_img_dirs(skycam_dir)
+    if original_fname[-4:] != '.jpg':
+        return None
+    if img_type == 'original':
+        return f'{original_sc2_dir}/{original_fname}'
+    elif img_type == 'cropped':
+        return f'{cropped_out_dir}/{original_fname[:-4]}_cropped.jpg'
+    elif img_type == 'pfov':
+        return f'{pfov_out_dir}/{original_fname[:-4]}_pfov.jpg'
 
 
 pixel_data_file = 'skycam_pixels.json'
-sc2_imgs = 'SC2_imgs_2023-08-15'
+sc2_img_dir = 'SC2_imgs_2023-08-01'
 
-corners_4x2 = get_corners()
-corners_4x1x2 = np.expand_dims(corners_4x2, axis=1)
-out_dir, cropped_img_out_dir, pfov_out_dir = init_dirs(sc2_imgs)
+corners_4x1x2 = get_corners()
+init_dirs(sc2_img_dir)
+original_sc2_dir, cropped_out_dir, pfov_out_dir = get_img_dirs(sc2_img_dir)
 
-for img_fname in os.listdir(sc2_imgs)[:10]:
+for original_fname in os.listdir(original_sc2_dir):
     # load the image
-    if img_fname[-4:] != '.jpg':
+    if original_fname[-4:] != '.jpg':
         continue
 
-    cropped_fname = f'{cropped_img_out_dir}/{img_fname[:-4]}_cropped.jpg'
-    pfov_fname = f'{pfov_out_dir}/{img_fname[:-4]}_pfov.jpg'
+    original_img = cv2.imread(get_img_path(original_fname, 'original', sc2_img_dir))
+    cropped_fpath = get_img_path(original_fname, 'cropped', sc2_img_dir)
+    pfov_fpath = get_img_path(original_fname, 'pfov', sc2_img_dir)
 
-    img = cv2.imread(f'{sc2_imgs}/{img_fname}')
-    crop_img(img, corners_4x1x2, cropped_fname)
-    plot_pfov(img, corners_4x1x2, pfov_fname)
+    crop_img(original_img, corners_4x1x2, cropped_fpath)
+    plot_pfov(original_img, corners_4x1x2, pfov_fpath)
 
