54e1944f873084c7fef15172148b9e6d376ea221
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2023-10-30 14:56:06 -0700

    Add file to automatically do a sparse checkout of the necessary files to run data labeling interface.

---
+++ cloud-detection/data_labeling/init_panoseti_data_labeling.sh
@@ -0,0 +1,32 @@
+if [ ! -d ".panoseti" ]; then
+    # Do a sparse git clone of the cloud-detection-USER branch
+    git clone --depth 1 \
+        --branch cloud-detection-USER \
+        --filter=blob:none \
+        --no-checkout \
+        --sparse \
+        https://github.com/panoseti/panoseti.git
+    cd panoseti
+    git sparse-checkout set cloud-detection/data_labeling
+
+    # Only checkout the necessary files for data labeling
+    git checkout cloud-detection-USER -- \
+        cloud-detection/data_labeling/PANOSETI\ Data\ Labeling\ Interface.ipynb \
+        cloud-detection/data_labeling/labeling_utils.py \
+        cloud-detection/data_labeling/label_session.py \
+        cloud-detection/data_labeling/skycam_utils.py \
+        cloud-detection/data_labeling/skycam_labels.json \
+        > /dev/null
+
+    # Create symbolic link to labeling interface sub-directory
+    cd ..
+    mv panoseti .panoseti
+    ln -s .panoseti/cloud-detection/data_labeling labeling
+
+    #echo "\033[32mAll files downloaded\033[0m"
+else
+    echo "The labeling directory already exists."
+fi
+
+
+
