077b911c8b8b592a22c634a8af427adbce91cc5f
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2023-11-01 11:30:48 -0700

    update script for running labeling docker.

--- cloud-detection/data_labeling/panoseti_data_labeling_docker/run_panoseti_data_labeling.sh
+++ cloud-detection/data_labeling/panoseti_data_labeling_docker/run_panoseti_data_labeling.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo docker run -it -d panoseti_data_labeling --name panoseti_data_labeling start.sh "jupyter notebook"
+#sudo docker run -it -d panoseti_data_labeling --name panoseti_data_labeling start.sh "jupyter notebook"
 #sudo docker run -i panoseti_data_labeling start.sh "jupyter notebook
-#sudo docker start -ia panoseti_data_labeling
+sudo docker start --attach -i panoseti_data_labeling
 
