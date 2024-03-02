Fix bugs, add upsampling option to pano training data builder, add precision-recall curve creation routine during training, update vision transformations to include random augmentations.

cloud-detection/dataset_construction/pano_builder.py
@@ -20,7 +20,7 @@ class PanoBatchBuilder(ObservingRunInterface, PanoBatchDataFileTree):
 
     raw_data_shapes = {
         'raw-original': (32, 32),
-        'raw-fft': (32, 32),
+        'raw-fft': (64, 64),
         'raw-derivative': (3, 32, 32),
         'raw-derivative.-60': (32, 32),
     }
@@ -233,7 +233,15 @@ class PanoBatchBuilder(ObservingRunInterface, PanoBatchDataFileTree):
         skycam_integration_offset = -60
         return self.module_file_time_seek(module_id, skycam_unix_t + skycam_integration_offset)
 
-    def create_training_features(self, feature_df, pano_df, module_id, skycam_df, skycam_dir, sample_stride, allow_skip=True):
+    def create_training_features(self, feature_df,
+                                 pano_df,
+                                 module_id,
+                                 skycam_df,
+                                 skycam_dir,
+                                 sample_stride,
+                                 upsample_pano_frames,
+                                 allow_skip=True,
+                                 ):
         """For each original skycam image:
             1. Get its unix timestamp.
             2. Find the corresponding panoseti image frame, if it exists.
@@ -250,89 +258,95 @@ class PanoBatchBuilder(ObservingRunInterface, PanoBatchDataFileTree):
         for index, skycam_row in skycam_info.sort_values(by='unix_t').iloc[::sample_stride, :].iterrows():
             skycam_uid, skycam_unix_t = skycam_row
             if self.verbose: print(f'\nGenerating pano features for skycam_uid {skycam_uid}...')
-            pano_frame_seek_info = self.correlate_skycam_to_pano_img(skycam_unix_t, module_id)
-            if pano_frame_seek_info is None:
-                if self.verbose: print('Failed to find matching panoseti frames. Skipping...')
-                continue
+            # Upsample panoset files to gain more features
+            if upsample_pano_frames:
+                target_times = np.linspace(skycam_unix_t - 20, skycam_unix_t + 20, 5)
+            else:
+                target_times = [skycam_unix_t]
+            for pano_target_unix_t in target_times:
+                pano_frame_seek_info = self.correlate_skycam_to_pano_img(pano_target_unix_t, module_id)
+                if pano_frame_seek_info is None:
+                    # if self.verbose: print('Failed to find matching panoseti frames. Skipping...')
+                    continue
 
-            # Generate all features
-            figs = dict()
-            figs['original'] = self.make_original_fig(
-                pano_frame_seek_info['file_idx'],
-                pano_frame_seek_info['frame_offset'],
-                module_id,
-                vmin=30,#-3.5,
-                vmax=300,#3.5,
-                cmap='mako',
-            )
-            figs['fft'] = self.make_fft_fig(
-                pano_frame_seek_info['file_idx'],
-                pano_frame_seek_info['frame_offset'],
-                module_id,
-                vmin=3,
-                vmax=10,
-                cmap='icefire',
-            )
-            figs['derivative'], figs['fft-derivative'] = self.make_time_derivative_figs(
-                pano_frame_seek_info['file_idx'],
-                pano_frame_seek_info['frame_offset'],
-                pano_frame_seek_info['frame_unix_t'],
-                module_id,
-                delta_ts=[-60, -40, -20],
-                vmin=[-150, -1],
-                vmax=[150, 6],
-                cmap=["icefire", "icefire"],
-            )
+                # Generate all features
+                figs = dict()
+                figs['original'] = self.make_original_fig(
+                    pano_frame_seek_info['file_idx'],
+                    pano_frame_seek_info['frame_offset'],
+                    module_id,
+                    vmin=30,#-3.5,
+                    vmax=300,#3.5,
+                    cmap='mako',
+                )
+                figs['fft'] = self.make_fft_fig(
+                    pano_frame_seek_info['file_idx'],
+                    pano_frame_seek_info['frame_offset'],
+                    module_id,
+                    vmin=3,
+                    vmax=10,
+                    cmap='icefire',
+                )
+                figs['derivative'], figs['fft-derivative'] = self.make_time_derivative_figs(
+                    pano_frame_seek_info['file_idx'],
+                    pano_frame_seek_info['frame_offset'],
+                    pano_frame_seek_info['frame_unix_t'],
+                    module_id,
+                    delta_ts=[-60, -40, -20],
+                    vmin=[-150, -1],
+                    vmax=[150, 6],
+                    cmap=["icefire", "icefire"],
+                )
 
-            # Check if all figs are valid
-            all_figs_valid = True
-            for img_type, fig in figs.items():
-                if fig is None:
-                    all_figs_valid = False
-                    msg = f'The following frame resulted in a None "{img_type}" figure: {pano_frame_seek_info}.'
-                    if not allow_skip:
-                        raise ValueError(msg)
-                    if self.verbose: print(msg)
+                # Check if all figs are valid
+                all_figs_valid = True
+                for img_type, fig in figs.items():
+                    if fig is None:
+                        all_figs_valid = False
+                        msg = f'The following frame resulted in a None "{img_type}" figure: {pano_frame_seek_info}.'
+                        if not allow_skip:
+                            raise ValueError(msg)
+                        if self.verbose: print(msg)
 
-            # Skip this image if not all figs are valid
-            if not all_figs_valid:
-                for fig in figs.values():
+                # Skip this image if not all figs are valid
+                if not all_figs_valid:
+                    for fig in figs.values():
+                        plt.close(fig)
+                    if self.verbose: print('Failed to create figures')
+                    # self.pano_dataset_builder.clear_current_entry()
+                    self.clear_current_entry()
+                    continue
+
+                # Write figures to data dirs
+                pano_fname = module_pff_files[pano_frame_seek_info['file_idx']]['fname']
+                frame_offset = pano_frame_seek_info['frame_offset']
+                pano_uid = get_pano_uid(pano_fname, frame_offset)
+                for img_type, fig in figs.items():
+                    if self.verbose: print(f"Creating {self.get_pano_img_path(pano_uid, img_type)}")
+                    fig.savefig(self.get_pano_img_path(pano_uid, img_type))
                     plt.close(fig)
-                if self.verbose: print('Failed to create figures')
-                # self.pano_dataset_builder.clear_current_entry()
-                self.clear_current_entry()
-                continue
 
-            # Write figures to data dirs
-            pano_fname = module_pff_files[pano_frame_seek_info['file_idx']]['fname']
-            frame_offset = pano_frame_seek_info['frame_offset']
-            pano_uid = get_pano_uid(pano_fname, frame_offset)
-            for img_type, fig in figs.items():
-                if self.verbose: print(f"Creating {self.get_pano_img_path(pano_uid, img_type)}")
-                fig.savefig(self.get_pano_img_path(pano_uid, img_type))
-                plt.close(fig)
+                # Commit entry to data_array for this run_dir
+                # self.pano_dataset_builder.write_arrays(pano_uid)
+                self.write_arrays(pano_uid)
 
-            # Commit entry to data_array for this run_dir
-            # self.pano_dataset_builder.write_arrays(pano_uid)
-            self.write_arrays(pano_uid)
-
-            # Update dataframes
-            pano_df = add_pano_img(
-                pano_df,
-                pano_uid,
-                self.run_dir,
-                pano_fname,
-                frame_offset,
-                module_id,
-                pano_frame_seek_info['frame_unix_t'],
-                self.batch_id
-            )
-            feature_df = add_feature_entry(
-                feature_df,
-                skycam_uid,
-                pano_uid,
-                self.batch_id
-            )
+                # Update dataframes
+                pano_df = add_pano_img(
+                    pano_df,
+                    pano_uid,
+                    self.run_dir,
+                    pano_fname,
+                    frame_offset,
+                    module_id,
+                    pano_frame_seek_info['frame_unix_t'],
+                    self.batch_id
+                )
+                feature_df = add_feature_entry(
+                    feature_df,
+                    skycam_uid,
+                    pano_uid,
+                    self.batch_id
+                )
         return feature_df, pano_df
 
     def create_inference_features(self, feature_df, pano_df, module_id, time_step, allow_skip=True, make_figs=False):
@@ -442,15 +456,16 @@ class PanoBatchBuilder(ObservingRunInterface, PanoBatchDataFileTree):
             )
         return feature_df, pano_df
 
-    def build_pano_training_batch_data(self, feature_df, pano_df, skycam_df, skycam_dir, sample_stride=1):
+    def build_pano_training_batch_data(self, feature_df, pano_df, skycam_df, skycam_dir, sample_stride=1, upsample_pano_frames=False):
         print(f'\nCreating panoseti features for {self.run_dir}')
         assert self.batch_type == 'training'
         for module_id in self.obs_pff_files:
-            if len(self.obs_pff_files[module_id]) == 0 or module_id == 3:
+            if len(self.obs_pff_files[module_id]) == 0 or module_id == 3 or module_id == 254:
                 continue
             try:
                 feature_df, pano_df = self.create_training_features(
-                    feature_df, pano_df, module_id, skycam_df, skycam_dir, sample_stride
+                    feature_df, pano_df, module_id, skycam_df, skycam_dir,
+                    sample_stride, upsample_pano_frames
                 )
             except FileExistsError:
                 continue

cloud-detection/dataset_construction/pano_utils.py
@@ -35,12 +35,13 @@ from batch_building_utils import *
 
 # Plotting
 def plot_fft_time_derivative(imgs, delta_ts, vmin, vmax, cmap):
-    for i in range(len(imgs)):
-        if imgs[i] is None or not isinstance(imgs[i], np.ndarray):
-            print('no image')
-            return None
-        if imgs[i].shape != (32, 32):
-            imgs[i] = np.reshape(imgs[i], (32, 32))
+    # for i in range(len(imgs)):
+    #     if imgs[i] is None or not isinstance(imgs[i], np.ndarray):
+    #         print('no image')
+    #         return None
+    #     if imgs[i].shape != (32, 32):
+    #         imgs[i] = np.reshape(imgs[i], (32, 32))
+
     # plt.rcParams.update({'axes.titlesize': 'small'})
     fig = plt.figure(figsize=(10., 3.))
 
@@ -100,11 +101,10 @@ def apply_fft(data):
         return None
     if data.shape != (32, 32):
         data = np.reshape(data, (32, 32))
-
     shape = data.shape
     window_type = "cosine"
     data = data * window(window_type, shape)
-    data = np.abs(fftn(data))
+    data = np.abs(fftn(data, (64, 64)))
     data = fftshift(data)
     try:
         data = np.log(data)

cloud-detection/model_training/cnn_model.py
@@ -111,7 +111,7 @@ class CloudDetection(nn.Module):
             nn.ReLU(),
             nn.BatchNorm1d(256),
 
-            nn.LazyLinear(4),
+            nn.LazyLinear(2),
         )
 
         # self.cnns = torch.nn.ModuleList([torch.nn.Sequential(self.conv1(), self.conv2(), self.conv3(), self.flatten()) for _ in range(3)])

cloud-detection/model_training/training_utils.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 import torch
-import torchvision
-from torchvision.transforms import v2
+# import torchvision
+# from torchvision.transforms import v2
+from sklearn.metrics import PrecisionRecallDisplay
 from torch import nn
 from torchsummary import summary
 
@@ -16,12 +17,12 @@ import seaborn as sns
 
 from PIL import Image
 import tqdm.notebook as tqdm
+from torchvision.transforms import v2
 
 # sys.path.append('../dataset_construction')
 # from model_training_utils import *
 
 
-img_type = 'raw-derivative.-60'
 
 # ---- Plotting ----
 plt.figure(figsize=(15, 10))
@@ -118,7 +119,17 @@ def weights_init(m):
 
 class Trainer:
 
-    def __init__(self, model, optimizer, loss_fn, train_loader, val_loader, epochs=1, gamma=0.9, do_summary=True):
+    def __init__(self, model,
+                 optimizer,
+                 loss_fn,
+                 train_loader,
+                 val_loader,
+                 epochs=1,
+                 gamma=0.9,
+                 do_summary=True,
+                 img_type='raw-derivative.-60',
+                 model_save_name='best_cloud_detection_model.pth'
+                 ):
         self.model = model
         self.optimizer = optimizer
         self.loss_fn = loss_fn
@@ -126,6 +137,11 @@ class Trainer:
         self.val_loader = val_loader
         self.epochs = epochs
         self.gamma = gamma
+        self.img_type = img_type
+        self.model_save_name = model_save_name
+
+        # self.bprc = BinaryPrecisionRecallCurve(thresholds=None)
+
         self.cloudy_wrong_data = []
         self.clear_wrong_data = []
         self.device = get_device()
@@ -156,7 +172,7 @@ class Trainer:
         self.model.eval()
         with torch.no_grad():
             img_data, y = next(iter(self.train_loader))
-            x = img_data[img_type]
+            x = img_data[self.img_type]
             x = x.to(device=self.device, dtype=torch.float)
             self.model(x)
         s = None
@@ -178,20 +194,24 @@ class Trainer:
         ncloudy_wrong = 0
         nclear_wrong = 0
         data_loader = self.train_loader if dataset_type == 'train' else self.val_loader
+        preds = torch.tensor([])
+        targets = torch.tensor([], dtype=int)
 
         self.model.eval()
         with torch.no_grad():
             for img_data, y in data_loader:
-                x = img_data[img_type]
+                x = img_data[self.img_type]
                 x = x.to(device=self.device, dtype=torch.float)
-                y = y.to(device=self.device, dtype=torch.long)
+                y = y.to(device=self.device)#, dtype=torch.long)
                 scores = self.model(x)
-
                 loss = self.loss_fn(scores, y)
                 loss_total += loss.item()
 
                 predictions = torch.argmax(scores, dim=1)
                 ncorrect += (predictions == y).sum()
+                # print(type(scores), type(y))
+                preds = torch.concatenate((preds, torch.amax(scores, dim=1).cpu()))
+                targets = torch.concatenate((targets, y.cpu()))
 
                 # if ((predictions == 1) & (predictions != y)).cpu().any():
                 #     for im, pred in zip(x, predictions):
@@ -220,6 +240,17 @@ class Trainer:
 
             report = "{0}: \tloss = {1:.4f},  acc = {2}/{3} ({4:.2f}%)".format(
                 dataset_type.capitalize().rjust(10), avg_loss, ncorrect, nsamples, acc * 100)
+            if dataset_type == 'val':
+                # print(preds, targets)
+                # self.bprc.update(preds, targets)
+                # self.bprc.plot(score=True)
+                # metrics.precision_recall_fscore_support(targets.numpy(), preds.numpy())
+                display = PrecisionRecallDisplay.from_predictions(
+                    targets, preds, name="CNN", plot_chance_level=True
+                )
+                _ = display.ax_.set_title("2-class Precision-Recall curve")
+                plt.show()
+                plt.close()
             return report
 
     def train(self):
@@ -239,42 +270,133 @@ class Trainer:
         # Init LR schedulers
         scheduler_exp = torch.optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=self.gamma)
         scheduler_plat = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer)
+        try:
+            for e in range(1, self.epochs + 1):
+                print(f"\n\nEpoch {e}")
+                for img_data, y in tqdm.tqdm(self.train_loader, unit="batches"):
+                    # Remove the gradients from the previous step
+                    self.optimizer.zero_grad()
+                    model.train()
+                    x = img_data[self.img_type]
+                    x = x.to(device=self.device, dtype=torch.float)
+                    y = y.to(device=self.device, dtype=torch.long)
 
-        for e in range(1, self.epochs + 1):
-            print(f"\n\nEpoch {e}")
-            for img_data, y in tqdm.tqdm(self.train_loader, unit="batches"):
-                model.train()
-                x = img_data[img_type]
-                x = x.to(device=self.device, dtype=torch.float)
-                y = y.to(device=self.device, dtype=torch.long)
+                    # Forward pass: compute class scores
+                    scores = model(x)
+                    loss = self.loss_fn(scores, y)
 
-                # Forward pass: compute class scores
-                scores = model(x)
-                loss = self.loss_fn(scores, y)
+                    # Backward pass: update weights
+                    loss.backward()
+                    self.optimizer.step()
 
-                # Remove the gradients from the previous step
-                self.optimizer.zero_grad()
+                # Update log of train and validation accuracy and loss. Print progress.
+                train_report = self.record_acc_and_loss('train')
+                valid_report = self.record_acc_and_loss('val')
+                print(valid_report, '\n', train_report)
 
-                # Backward pass: update weights
-                loss.backward()
-                self.optimizer.step()
+                # Save model parameters with the best validation accuracy
+                val_accs = self.training_log['val']['acc']
+                if val_accs[-1] == max(val_accs):
+                    torch.save(model.state_dict(), f"../model_training/{self.model_save_name}")
 
-            # Update log of train and validation accuracy and loss. Print progress.
-            train_report = self.record_acc_and_loss('train')
-            valid_report = self.record_acc_and_loss('val')
-            print(valid_report, '\n', train_report)
+                # Update optimizer
+                scheduler_exp.step()
+                scheduler_plat.step(self.training_log['val']['loss'][-1])
+            print('Done training')
+            self.make_training_plots(do_save=True)
+        except KeyboardInterrupt:
+            print('Keyboard Interrupt: Stopping training')
+            # self.make_training_plots(do_save=False)
 
-            # Save model parameters with the best validation accuracy
-            val_accs = self.training_log['val']['acc']
-            if val_accs[-1] == max(val_accs):
-                torch.save(model.state_dict(), "../model_training/best_cloud_detection_model.pth")
-
-            # Update optimizer
-            scheduler_exp.step()
-            scheduler_plat.step(self.training_log['val']['loss'][-1])
-        print('Done training')
-        plot_accuracy(self.training_log)
-        plot_loss(self.training_log)
-        plot_cloudy_mistakes(self.training_log)
-        plot_clear_mistakes(self.training_log)
+    def make_training_plots(self, do_save):
+        plot_accuracy(self.training_log, save=do_save)
+        plt.show()
         plt.close()
+        plot_loss(self.training_log, save=do_save)
+        plt.show()
+        plt.close()
+        plot_cloudy_mistakes(self.training_log, save=do_save)
+        plt.show()
+        plt.close()
+        plot_clear_mistakes(self.training_log, save=do_save)
+        plt.show()
+        plt.close()
+
+# if __name__ == '__main__':
+#     from cnn_model import CloudDetection
+#     from data_loaders import CloudDetectionTrain
+#
+#     transform = v2.Compose([
+#         v2.ToTensor(),
+#         v2.RandomHorizontalFlip(),
+#         v2.RandomVerticalFlip(),
+#         v2.Normalize(mean=[0], std=[67]),
+#         v2.ToDtype(torch.float, scale=True),
+#         # v2.ColorJitter(0.5, None, None, None),
+#         v2.GaussianBlur(kernel_size=(5, 5), sigma=(0.2, 0.5))
+#     ])
+#
+#     # torchvision.transforms.ToTensor()
+#
+#     test_prop = 0.0
+#     val_prop = 0.3
+#
+#     labeled_data = CloudDetectionTrain(
+#         transform=transform
+#     )
+#     dataset_size = len(labeled_data)
+#     dataset_indices = np.arange(dataset_size)
+#
+#     np.random.shuffle(dataset_indices)
+#
+#     # Test / Train split
+#     test_split_index = int(np.floor(test_prop * dataset_size))
+#     trainset_indices, test_idx = dataset_indices[test_split_index:], dataset_indices[:test_split_index]
+#
+#     # Train / Val split
+#     trainset_size = len(trainset_indices)
+#     val_split_index = int(np.floor(val_prop * trainset_size))
+#     train_idx, val_idx = trainset_indices[val_split_index:], trainset_indices[:val_split_index]
+#
+#     batch_size = 64
+#
+#     # NUM_TRAIN = int(len(labeled_data) * proportion_train)
+#     # NUM_TRAIN = NUM_TRAIN - NUM_TRAIN % batch_size
+#
+#     # val_split_index = int(np.floor(proportion_val * dataset_size))
+#     # train_idx, val_idx = dataset_indices[val_split_index:], dataset_indices[:val_split_index]
+#     test_loader = torch.utils.data.DataLoader(
+#         dataset=labeled_data,
+#         batch_size=batch_size,
+#         sampler=torch.utils.data.SubsetRandomSampler(test_idx)
+#     )
+#
+#     train_loader = torch.utils.data.DataLoader(
+#         dataset=labeled_data,
+#         batch_size=batch_size,
+#         sampler=torch.utils.data.SubsetRandomSampler(train_idx)
+#     )
+#
+#     val_loader = torch.utils.data.DataLoader(
+#         dataset=labeled_data,
+#         batch_size=batch_size,
+#         sampler=torch.utils.data.sampler.SubsetRandomSampler(val_idx)
+#     )
+#
+#     img_type = 'raw-derivative.-60'
+#
+#     learning_rate = 0.001
+#     # momentum=0.9
+#
+#     model = CloudDetection()
+#
+#     # optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=1e-6)#, momentum=momentum)
+#     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+#     loss_fn = nn.CrossEntropyLoss()
+#
+#     trainer = Trainer(
+#         model, optimizer, loss_fn, train_loader, val_loader,
+#         epochs=30, gamma=0.9, do_summary=False,
+#         img_type=img_type
+#     );
+#     trainer.train()