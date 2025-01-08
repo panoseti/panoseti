64aec45a8e53268bf433d875f7cd72d7c3e3ac6d
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2025-01-07 21:38:00 -0800

    anom_det: implement vae training function and dataset visualization for ph data.

---
+++ cloud-detection/anomaly_detection/ph_dataset.py
@@ -0,0 +1,151 @@
+import sys, os
+import logging, typing
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+import torch
+
+sys.path.append('../../util')
+import panoseti_file_interfaces as pfi
+import pff
+from vae_model import *
+
+
+
+class PulseHeightDataset(torch.utils.data.Dataset):
+    """Interface for retrieving pulse-height images from a specific observing run."""
+    MAX_PH_VAL = 2**16 - 1  # Max PH pixel value. PH pixels are typically represented as uint16 values.
+    OUTLIER_CUTOFF = MAX_PH_VAL - 1000  # Value defining pixel outlier status
+
+    def __init__(self, config: typing.Dict, transform=None, target_transform=None, log_level=logging.ERROR):
+        super().__init__()
+        self.transform = transform
+        # Validate configuration dict
+        assert {'data_dir', 'run_dir', 'module_ids'}.issubset(set(config.keys()))
+        assert len(config['module_ids']) > 0
+        self.config = config
+        self.ori = pfi.ObservingRunInterface(config['data_dir'], config['run_dir'])
+        assert self.ori.obs_pff_files is not None
+        assert config['module_ids'] == 'all' or set(config['module_ids']).issubset(set(self.ori.obs_pff_files.keys()))
+        
+        # Initialize PH frame generator
+        if config['module_ids'] == 'all':
+            self.dataset_module_ids = self.ori.obs_pff_files.keys()
+        else:
+            self.dataset_module_ids = config['module_ids']
+        self.ph_gen = self.ph_generator()
+
+        self.logger = logging.getLogger("PulseHeightDataset")
+        self.logger.setLevel(log_level)
+        self.iter_num = 0
+        self.length = len(self)
+
+    def ph_generator(self):
+        """Returns a generator that yields PH frames."""
+        self.__ph_generators = {module_id: self.load_ph_data_lazy(module_id) for module_id in self.dataset_module_ids}
+        for module_id in self.dataset_module_ids:
+            for j in self.__ph_generators[module_id]:
+                ph_img = j['img_data']
+                del j['img_data']
+                ph_img_clean, max_pixel_val = self.clean_ph_img(ph_img)
+                if ph_img_clean is None:
+                    continue
+                yield {'meta': j, 'img': ph_img_clean, 'max_pixel_val': max_pixel_val}
+    
+    def load_ph_data_lazy(self, module_id: int):
+        """Sequentially yields PH frames from module_id."""
+        assert hasattr(self, 'ori')
+        if module_id not in self.ori.obs_pff_files:
+            print(f'No module with ID "{module_id}"\n'
+                  f'Available module_ids:\n\t{list(self.ori.obs_pff_files.keys())}')
+            return None
+        for ph_file in self.ori.obs_pff_files[module_id]["ph"]:
+            fname = ph_file["fname"]
+            fpath = "{0}/{1}/{2}".format(self.config['data_dir'], self.config['run_dir'], fname)
+            with open(fpath, 'rb') as fp:
+                frame_iterator = self.ori.pulse_height_frame_iterator(fp, 1)
+                for j, img in frame_iterator:
+                    j['wr_timestamp (s)'] = pff.wr_to_unix_decimal(j['pkt_tai'], j['pkt_nsec'], j['tv_sec'])
+                    j['unix_timestamp'] = pd.to_datetime(float(j['wr_timestamp (s)']), unit = 's', utc=True)
+                    j['img_data'] = img
+                    yield j
+
+    def clean_ph_img(self, ph_img: np.ndarray, outlier_strategy='zero', clip_z_score=5) -> typing.Tuple[int, int]:
+        """Set outlier pulse heigh pixel values to 0.
+        Arguments:
+            ph_img: raw pulse-height image
+            outlier_strategy: how to deal with outliers
+                'zero' => set outliers to 0
+                'clip' => clip outlier values to a max pixel value given by clip_z_score and inlier mean and standard deviations.
+        Returns: 2-element tuple:
+            idx 0: pulse-height frame after applying the specified outlier_strategy with pixels normalized to 0-1 
+            idx 1: maximum max_pixel_val, represented by the pixel value 1.0
+        """
+        # Remove outliers
+        assert outlier_strategy in ['zero', 'clip']
+        if outlier_strategy == 'zero':
+            ph_img_clean = ph_img.copy()
+            ph_img_clean[ph_img >= self.OUTLIER_CUTOFF] = 0
+        elif outlier_strategy == 'clip':
+            inlier_mean = np.mean(ph_img[ph_img < self.OUTLIER_CUTOFF])
+            inlier_std = np.std(ph_img[ph_img < self.OUTLIER_CUTOFF])
+            clip_max_pixel_val = inlier_mean + clip_z_score * inlier_std
+            ph_img_clean = ph_img.copy()
+            ph_img_clean = np.clip(ph_img_clean, 0, clip_max_pixel_val)
+    
+        # PH image should not be all zeros
+        max_pixel_val = np.max(ph_img_clean)
+        if max_pixel_val == 0:
+            self.logger.warning('All PH pixels are zero')
+            return None, 0
+        ph_img_clean = ph_img_clean / max_pixel_val
+        return ph_img_clean, max_pixel_val
+
+    def get_ph_data(self, index: int):
+      if self.iter_num < self.length:
+        self.iter_num += 1
+        try:
+            return next(self.ph_gen)
+        except StopIteration:
+            self.ph_gen = self.ph_generator()
+            return next(self.ph_gen)
+      else:
+        self.iter_num = 0
+        self.ph_gen = self.ph_generator()
+        return next(self.ph_gen)
+            
+    def __getitem__(self, index: int) -> torch.Tensor:
+        """Get PH frame at index. Note: currently index has no effect (TODO). index is required by the PyTorch abstract Dataset class."""
+        return self.transform(self.get_ph_data(index)['img'])
+
+    def __len__(self) -> int:
+        total_ph_frames = 0
+        for module_id in self.dataset_module_ids:
+            for ph_file in self.ori.obs_pff_files[module_id]['ph']:
+                total_ph_frames += ph_file['nframes']
+        return total_ph_frames // 100
+
+
+
+# PH data EDA: visualize PH image and the distribution of pixel values.
+def plot_ph_pixel_dist(img, max_pixel_val, meta, ax=None):
+    """Plot pulse height pixel distribution (for EDA outlier rejection)."""
+    sns.histplot(img.ravel() * max_pixel_val, stat='density', ax=ax)
+    if ax:
+        ax.set_title(f"Distribution of PH pixels from Q{meta['quabo_num']} at \n{meta['unix_timestamp']}")
+
+def plot_ph_img(img, max_pixel_val, meta, ax=None):
+    if ax is None:
+        f = plt.figure()
+        ax = plt.gca()
+    mu = np.mean(img)
+    std = np.std(img)
+    sigma = 0
+    low = (mu + std * sigma) * max_pixel_val
+    img_plt = ax.imshow(img * max_pixel_val, cmap='rocket')
+    plt.colorbar(img_plt, fraction=0.045)
+
+
--- cloud-detection/anomaly_detection/vae_model.py
+++ cloud-detection/anomaly_detection/vae_model.py
@@ -74,7 +74,7 @@ class Flatten(nn.Module):
         return x
 
 
-class Unflatten(nn.Module):
+class Unflatten32x32(nn.Module):
     def __init__(self, in_channels: int):
         super().__init__()
         self.conv_inv = nn.ConvTranspose2d(
@@ -82,6 +82,25 @@ class Unflatten(nn.Module):
             out_channels=in_channels,
             kernel_size=7,
             stride=7,
+            padding=0,
+        )
+        self.bn = nn.BatchNorm2d(num_features=in_channels)
+        self.gelu = nn.GELU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv_inv(x)
+        x = self.bn(x)
+        x = self.gelu(x)
+        return x
+
+class Unflatten16x16(nn.Module):
+    def __init__(self, in_channels: int):
+        super().__init__()
+        self.conv_inv = nn.ConvTranspose2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=1,
+            stride=1,
             padding=0,
         )
         self.bn = nn.BatchNorm2d(num_features=in_channels)
@@ -159,7 +178,7 @@ class FCBlock(nn.Module):
 
 
 # Define the VAE model
-class VaeModelVersion2(nn.Module):
+class VaeModel32x32(nn.Module):
     def __init__(self, in_channels: int, hidden_dim=64, latent_dim=16):
         super().__init__()
         self.hidden_dim = hidden_dim
@@ -176,7 +195,7 @@ class VaeModelVersion2(nn.Module):
 
         # Decoder
         self.fc_dec = FCBlock(latent_dim, 2 * hidden_dim * 8**2)
-        self.unflatten = Unflatten(2*hidden_dim)
+        self.unflatten = Unflatten32x32(2*hidden_dim)
         self.up_block_8_to_16 = UpBlock(2*hidden_dim, hidden_dim)
         self.up_block_16_to_32 = UpBlock(hidden_dim, hidden_dim)
         self.conv_block_output = ConvBlock(hidden_dim, hidden_dim)
@@ -225,61 +244,64 @@ class VaeModelVersion2(nn.Module):
         recon_x = self.decode(z)
         return recon_x, mu, logvar
 
-
-
 # Define the VAE model
-class VaeModel(nn.Module):
-    def __init__(self, in_channels: int, latent_dim=16):
+class VaeModel16x16(nn.Module):
+    def __init__(self, in_channels: int, hidden_dim=64, latent_dim=16):
         super().__init__()
+        self.hidden_dim = hidden_dim
         self.latent_dim = latent_dim
         self.in_channels = in_channels
         
         # Encoder
-        self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels, 48, kernel_size=5, stride=2, padding=1),  # (48, 16, 16)
-            nn.BatchNorm2d(48),
-            nn.GELU(),
-            nn.Conv2d(48, 64, kernel_size=5, stride=2, padding=1), # (64, 8, 8)
-            nn.BatchNorm2d(64),
-            nn.GELU(),
-            nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=1), # (128, 4, 4)
-            nn.BatchNorm2d(128),
-            nn.GELU()
-        )
-        
-        self.fc_mu = nn.Linear(128 * 3 * 3, latent_dim)  # Mean of latent space
-        self.fc_logvar = nn.Linear(128 * 3 * 3, latent_dim)  # Log variance of latent space
-        
+        self.conv_block_input = ConvBlock(in_channels, hidden_dim)
+        self.down_block_16_to_8 = DownBlock(hidden_dim, 2*hidden_dim)
+        self.flatten = Flatten()
+        self.fc_mu = FCBlock(2 * hidden_dim, latent_dim)
+        self.fc_logvar = FCBlock(2 * hidden_dim, latent_dim)
+
         # Decoder
-        self.fc_dec = nn.Linear(latent_dim, 128 * 3 * 3)
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2, padding=1), # (64, 8, 8)
-            nn.BatchNorm2d(64),
-            nn.GELU(),
-            nn.ConvTranspose2d(64, 48, kernel_size=5, stride=2, padding=1), # (48, 16, 16)
-            nn.BatchNorm2d(48),
-            nn.GELU(),
-            nn.ConvTranspose2d(48, in_channels, kernel_size=4, stride=2, padding=0), # (C, 32, 32)
-            # nn.Sigmoid()  # Output in range [0, 1]
+        self.fc_dec = FCBlock(latent_dim, 2 * hidden_dim * 8**2)
+        self.unflatten = Unflatten16x16(2*hidden_dim)
+        self.up_block_8_to_16 = UpBlock(2*hidden_dim, hidden_dim)
+        self.conv_block_output = ConvBlock(hidden_dim, hidden_dim)
+        self.conv_op_311 = nn.Conv2d(
+            in_channels=hidden_dim,
+            out_channels=1,
+            kernel_size=3,
+            stride=1,
+            padding=1,
         )
-    
+        
     def encode(self, x):
-        x = self.encoder(x)
-        x = x.view(x.size(0), -1)  # Flatten
-        mu = self.fc_mu(x)
-        logvar = self.fc_logvar(x)
+        """Compresses x into a latent space representation."""
+        # Forward through U-Net
+        # print(x.shape)
+        conv_block_1 = self.conv_block_input(x)
+        down_1 = self.down_block_16_to_8(conv_block_1)
+        flatten = self.flatten(down_1)
+        flatten = flatten.view(flatten.size(0), -1)
+        mu = self.fc_mu(flatten)
+        logvar = self.fc_logvar(flatten)
         return mu, logvar
     
     def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)  # Standard deviation
-        eps = torch.randn_like(std)  # Sample epsilon from N(0,1)
-        return mu + eps * std
+        """Separate the random sample from the learnable parameters."""
+        std = torch.exp(0.5 * logvar)
+        epsilon = torch.randn_like(std)
+        return mu + epsilon * std
     
     def decode(self, z):
         x = self.fc_dec(z)
-        x = x.view(x.size(0), 128, 3, 3)  # Reshape
-        x = self.decoder(x)
-        return x
+        # Decode 
+        x = x.view(x.size(0), 2*self.hidden_dim, 8, 8)
+        unflatten = self.unflatten(x)
+        up_1 = self.up_block_8_to_16(unflatten)
+        # print('up_1 shape', up_1.shape)
+        conv_block_1 = self.conv_block_output(up_1)
+        # print('conv_block_1 shape', conv_block_1.shape)
+        recon_x = self.conv_op_311(conv_block_1)
+        # print('x recon shape:', recon_x.shape)
+        return recon_x
     
     def forward(self, x):
         mu, logvar = self.encode(x)
