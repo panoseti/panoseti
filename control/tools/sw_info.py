5306bf336aeeec28beacdcecb0d5a6b6ae6689ea
Wei Liu <liuwei_berkeley@berkeley.edu>
2025-07-27 15:07:47 -0700

    added the two scripts to tools.

--- control/tools/sw_info.py
+++ control/tools/sw_info.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 import git
 import json
-
+from utils import config_file
 def get_sw_info():
     repo = git.Repo(search_parent_directories=True)
     commit = repo.head.commit.hexsha
@@ -12,7 +12,7 @@ def get_sw_info():
              'author':author,\
              'branch':branch,
              'commit_date':commit_date }
-    with open('sw_info.json','w') as f:
+    with open(config_file.sw_info_filename,'w') as f:
         json.dump(sw_info, f, indent=4)
 
 if __name__ == '__main__':
