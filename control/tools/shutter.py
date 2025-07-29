df1e6b63642d2b3f759ff9553c1ab4074d5f2d63
Wei Liu <liuwei_berkeley@berkeley.edu>
2025-07-29 11:04:34 -0700

    use port forwarding in shutter.py.

--- control/tools/shutter.py
+++ control/tools/shutter.py
@@ -11,9 +11,16 @@ from driver.quabo_tftp import tftpw
 from driver import quabo_driver
 from utils import config_file, pixel_coords
 from argparse import ArgumentParser
+import logging
 # check the ip address
 # shutter controller is connected to quabo1
 # 
+if not os.path.exists('logs'):
+        os.makedirs('logs')
+logfile = 'logs/shutter.log'
+util.create_logger(logfile, 'PANOSETI.Shutter', 'a')
+logger = logging.getLogger('PANOSETI.Shutter')
+
 def ip_check(ip):
     ip_str = ip.split('.')
     if(int(ip_str[3])%4==1):
@@ -23,21 +30,24 @@ def ip_check(ip):
     
 # shutter operation
 #
-def shutterop(ip,op):
+def shutterop(ip, port, op):
     # ip: ip address of quabo
+    # port: port used for communicating with Quabo.
     # op: 0--open the shutter
     #     1--close the shutter
     if(op):
         opstr = 'close'
     else:
         opstr = 'open'
-    print('%s shutter on %s'%(opstr,ip))
-    quabo = quabo_driver.QUABO(ip)
+    logger.info('%s shutter on %s:%d'%(opstr,ip, port))
+    quabo = quabo_driver.QUABO(ip, port)
     quabo.shutter_new(op)
 
 def main():
+    logger.info('************************************')
     parser = ArgumentParser(description="Usage for openning/closing shutter.")
     parser.add_argument("--ip",type=str, dest="ip",help="ip address of the quabo")
+    parser.add_argument("--port",type=int, dest="port", default=60000, help="port used for communicating with Quabo.")
     parser.add_argument("--open", dest="open",action="store_true", help="open the shutter")
     parser.add_argument("--close", dest="close",action="store_true", help="close the shutter")
     opts = parser.parse_args()
@@ -46,15 +56,19 @@ def main():
     if(opts.close):
         op = 1
     if(opts.ip):
-        if(ip_check(opts.ip)):
-            raise Exception('Please make sure the IP address is correct.')
-        shutterop(opts.ip,op)
+        shutterop(opts.ip, opts.port, op)
         return
     obs_config = config_file.get_obs_config()
+    network_config = config_file.get_network_config()
     for dome in obs_config['domes']:
         for m in dome['modules']:
             ip = config_file.quabo_ip_addr(m['ip_addr'],1)
-            shutterop(ip,op)
+            ip_ports = util.get_quabo_ip_port(m['ip_addr'], 1, network_config)
+            real_ip = ip_ports['ip_addr']
+            real_port = ip_ports['cmd_port']
+            logger.debug('Quabo IP: %s'%real_ip)
+            logger.debug('Real IP: %d'%real_port)
+            shutterop(real_ip, real_port, op)
 
 if __name__ == "__main__":
     main()
