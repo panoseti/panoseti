7f94a015d5a3394e82f7a51ef6643542c34f4eb1
Wei Liu <liuwei_berkeley@berkeley.edu>
2025-08-01 12:15:02 -0700

    removed util.write_log, as we use logging package for the logs.

--- control/tools/hv_updater.py
+++ control/tools/hv_updater.py
@@ -57,7 +57,6 @@ def get_adjusted_detector_hv(det_serial_num: str, temp: float) -> float:
         msg = "hv_updater: Failed to get the nominal HV for the detector with serial number: '{0}'."
         msg += "detector_info.json might be missing an entry for this detector. "
         msg += "Error msg: {1}"
-        #util.write_log(msg.format(det_serial_num, kerr))
         logger.error(msg.format(det_serial_num, kerr))
         raise
     else:
@@ -82,7 +81,6 @@ def update_quabo(quabo_obj: quabo_driver.QUABO,
     except KeyError as kerr:
         msg = "A detector in the quabo with IP {0} could not be found in the configuration files. "
         msg += "Error message: {1}"
-        #util.write_log(msg.format(quabo_obj.ip_addr, kerr))
         logger.error(msg.format(quabo_obj.ip_addr, kerr))
         raise
     else:
@@ -98,13 +96,11 @@ def get_redis_temp(r: redis.Redis, rkey: str) -> float:
     except redis.RedisError as err:
         msg = "hv_updater: A Redis error occurred. "
         msg += "Error msg: {0}"
-        #util.write_log(msg.format(err))
         raise
     except TypeError as terr:
         msg = "hv_updater: Failed to update '{0}'. "
         msg += "Temperature HK data may be missing. "
         msg += "Error msg: {1}"
-        #util.write_log(msg.format(rkey, terr))
         raise
 
 
@@ -152,19 +148,16 @@ def update_all_quabos(r: redis.Redis):
                 except Warning as werr:
                     msg = "hv_updater: Failed to update quabo at index {0} with base IP {1}. "
                     msg += "Error msg: {2} \n"
-                    #util.write_log(msg.format(quabo_index, module_ip_addr, werr))
                     logger.error(msg.format(quabo_index, module_ip_addr, werr))
                     continue
                 except redis.RedisError as rerr:
                     msg = "hv_updater: A Redis error occurred. "
                     msg += "Error msg: {0}"
-                    #util.write_log(msg.format(rerr))
                     logger.error(msg.format(rerr))
                     raise
                 except KeyError as kerr:
                     msg = "hv_updater: Quabo {0} with base IP {1} may be missing from a config file. "
                     msg += "Error msg: {2}"
-                    #util.write_log(msg.format(quabo_index, module_ip_addr, kerr))
                     logger.error(msg.format(quabo_index, module_ip_addr, kerr))
                     raise
                 except OSError as oserr:
@@ -178,17 +171,14 @@ def update_all_quabos(r: redis.Redis):
                         msg = "hv_updater: The temperature of quabo {0} with base IP {1} is {2} C, "
                         msg += "which exceeds the maximum operating temperatures. \n"
                         msg += "Attempting to power down the detectors on this quabo..."
-                        #util.write_log(msg.format(quabo_index, module_ip_addr, temp))
                         logger.info(msg.format(quabo_index, module_ip_addr, temp))
                         try:
                             quabo_obj.hv_set([0] * 4)
                             quabos_off.add(rkey)
-                            #util.write_log("Successfully powered down.")
                             logger.info('Successfully powered down.')
                         except Exception as err:
                             msg = "*** hv_updater: Failed to power down detectors. "
                             msg += "Error msg: {0}"
-                            #util.write_log(msg.format(err))
                             logger.error(msg.format(err))
                             continue
                 # TODO: Determine when (or if) we should turn detectors back on after a temperature-related power down.
@@ -222,6 +212,5 @@ if __name__ == "__main__":
         main()
     except Exception as e:
         msg = "hv_updater failed and exited with the error message: {0}"
-        #util.write_log(msg.format(e))
         logger.error(msg.format(e))
         raise
